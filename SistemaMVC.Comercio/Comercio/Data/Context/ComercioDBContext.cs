ae0cc074f15d000e1d3113c3085e9b2559132221
bonotto <rafareisaba@gmail.com>
2021-11-13 01:29:48 -0300

    Classe ComercioDBContext

---
+++ SistemaMVC.Comercio/Comercio/Data/Context/ComercioDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sistema.Comercio.Data.Context
+{
+    public class ComercioDBContext : DbContext
+    {
+        public ComercioDBContext(DbContextOptions<ComercioDBContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ComercioDBContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
