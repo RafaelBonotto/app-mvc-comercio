604f237fd1bdd05c690c9c13cd0722414c536a0f
bonotto <rafareisaba@gmail.com>
2021-11-13 01:48:37 -0300

    Criação da interface IRepositoryBase

---
+++ SistemaMVC.Comercio/Comercio/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Comercio.Interfaces
+{
+    public interface IRepositoryBase<T>
+    {
+        Task<List<T>> GetAllAsync();
+        Task<List<T>> GetByKeyAsync(string key);        
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        Task<T> DeleteAsync(int id);
+        Task<T> UpdateAsync(T entity);
+    }
+}
