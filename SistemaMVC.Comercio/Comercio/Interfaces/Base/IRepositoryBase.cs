4d550e76a4d1cfc4293acb8d4ca5ccf6099a98d8
RafaelBonotto <rafareisaba@outlook.com>
2021-11-16 23:53:11 -0300

    Método GetAllFiteredAsync

--- SistemaMVC.Comercio/Comercio/Interfaces/Base/IRepositoryBase.cs
+++ SistemaMVC.Comercio/Comercio/Interfaces/Base/IRepositoryBase.cs
@@ -6,6 +6,7 @@ namespace Comercio.Interfaces.Base
     public interface IRepositoryBase<T>
     {
         Task<List<T>> GetAllAsync();
+        Task<List<T>> GetAllFilteredAsync(T entity);
         Task<List<T>> GetByKeyAsync(string key);        
         Task<T> GetByIdAsync(int id);
         Task<T> AddAsync(T entity);
