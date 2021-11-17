57817a07662ede0eb0724ad9f3bea4689cfcc82e
RafaelBonotto <rafareisaba@outlook.com>
2021-11-16 23:55:42 -0300

    Criação da Model Fornecedor

---
+++ SistemaMVC.Comercio/Comercio/Models/Fornecedor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Comercio.Models
+{
+    public class Fornecedor
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Fornecedor")]
+        public string Nome_empresa { get; set; }
+
+        [Display(Name = "Telefone")]
+        [MaxLength(12)]
+        public string Telefone_empresa { get; set; }
+
+        [Display(Name = "Vendedor")]
+        public string Nome_vendedor { get; set; }
+
+        [Display(Name = "Telefone")]
+        [MaxLength(12)]
+        public string Telefone_vendedor { get; set; }
+        public sbyte Ativo { get; set; }
+        public DateTime Data_criacao { get; set; }
+        public DateTime Data_alteracao { get; set; }
+    }
+}
