52905fed29c5460fe1c6a17424974660cf857666
RafaelBonotto <rafareisaba@outlook.com>
2022-03-05 01:34:29 -0300

    Create ProdutoQuery.cs

---
+++ SistemaMVC.Comercio/Comercio/Data/Querys/ProdutoQuery.cs
@@ -0,0 +1,21 @@
+﻿namespace Comercio.Data.Querys
+{
+    public static class ProdutoQuery
+    {
+        public const string SELECT_POR_CODIGO = @"SELECT* FROM 
+                                                  tb_produto WHERE 
+                                                  codigo = @codigo";
+
+        public const string SELECT_POR_DESCRICAO = @"SELECT* FROM 
+                                                        tb_produto WHERE 
+                                                        descricao LIKE CONCAT('%',@descricao,'%')";
+
+        public const string SELECT_POR_SETOR_ID = @"SELECT* FROM 
+                                                        tb_produto WHERE 
+                                                        setor_id = @setor_id";
+
+        public const string SELECT_POR_ID = @"SELECT* FROM 
+                                                tb_produto WHERE 
+                                                id = @id";
+    }
+}
