6a06e19429f48173e838469dae0773df0871df61
RafaelBonotto <rafareisaba@outlook.com>
2022-03-04 23:27:32 -0300

    Create IMySqlConnectionManager.cs

---
+++ SistemaMVC.Comercio/Comercio/Data/ConnectionManager/IMySqlConnectionManager.cs
@@ -0,0 +1,10 @@
+﻿using MySqlConnector;
+using System.Threading.Tasks;
+
+namespace Comercio.Data.ConnectionManager
+{
+    public interface IMySqlConnectionManager
+    {
+        Task<MySqlConnection> GetConnectionAsync();
+    }
+}
