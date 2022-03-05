4e16e0e6062e0baf2638efcdffaf549fecd93a7a
RafaelBonotto <rafareisaba@outlook.com>
2022-03-04 23:27:38 -0300

    Create MySqlConnectionManager.cs

---
+++ SistemaMVC.Comercio/Comercio/Data/ConnectionManager/MySqlConnectionManager.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySqlConnector;
+using System;
+using System.Threading.Tasks;
+
+namespace Comercio.Data.ConnectionManager
+{
+    public class MySqlConnectionManager : IMySqlConnectionManager
+    {
+        private readonly IConfiguration _config;
+        private readonly string _connectionString;
+
+        public MySqlConnectionManager(IConfiguration config)
+        {
+            _config = config;
+            _connectionString = _config.GetSection("ConnectionStrings:comercioConection").Value;
+        }
+
+        public async Task<MySqlConnection> GetConnectionAsync()
+        {
+            try
+            {
+                MySqlConnection connection = new(_connectionString);
+                await connection.OpenAsync();
+                return connection;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
