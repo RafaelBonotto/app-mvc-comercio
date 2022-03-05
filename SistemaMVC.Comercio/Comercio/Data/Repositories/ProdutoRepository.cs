a9788a8a2725115d474012a6c7649c9281db2c37
RafaelBonotto <rafareisaba@outlook.com>
2022-03-04 23:27:59 -0300

    Update ProdutoRepository.cs

--- SistemaMVC.Comercio/Comercio/Data/Repositories/ProdutoRepository.cs
+++ SistemaMVC.Comercio/Comercio/Data/Repositories/ProdutoRepository.cs
@@ -1,7 +1,7 @@
-﻿using Comercio.Data.Context;
+﻿using Comercio.Data.ConnectionManager;
 using Comercio.Interfaces.Base;
 using Comercio.Models;
-using Microsoft.EntityFrameworkCore;
+using Dapper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,116 +11,41 @@ namespace Comercio.Data.Repositories
 {
     public class ProdutoRepository : IRepositoryBase<Produto>
     {
-        private readonly ComercioDBContext _context;
+        private readonly IMySqlConnectionManager _connection;
 
-        public ProdutoRepository(ComercioDBContext context)
+        public ProdutoRepository(IMySqlConnectionManager connection)
         {
-            _context = context;
+            _connection = connection;
         }
 
-        public async Task<Produto> AddAsync(Produto produto)
+        public Task<Produto> AddAsync(Produto entity)
         {
-            using (var transaction = await _context.Database.BeginTransactionAsync())
-            {
-                try
-                {
-                    produto.Ativo = 1;
-                    produto.Data_criacao = DateTime.Now;
-                    produto.Data_alteracao = DateTime.Now;
-                    produto.Data_fabricacao = DateTime.Now;
-                    produto.Data_validade = DateTime.Now;
-                    _context.TB_PRODUTO.Add(produto);
-                    await _context.SaveChangesAsync();
-                    await transaction.CommitAsync();
-                    return produto;
-                }
-                catch (Exception)
-                {
-                    await transaction.RollbackAsync();
-                    throw;
-                }
-            }
+            throw new NotImplementedException();
         }
 
-        public async Task<Produto> DeleteAsync(int id)
+        public Task<Produto> DeleteAsync(int id)
         {
-            using (var transaction = await _context.Database.BeginTransactionAsync())
-            {
-                try
-                {
-                    var produto = _context.TB_PRODUTO.Find(id);
-                    produto.Ativo = 0;
-                    produto.Data_alteracao = DateTime.Now;
-                    await _context.SaveChangesAsync();
-                    await transaction.CommitAsync();
-                    return produto;
-                }
-                catch (Exception)
-                {
-                    await transaction.RollbackAsync();
-                    throw;
-                }
-            }
+            throw new NotImplementedException();
         }
 
-        public async Task<List<Produto>> GetAllAsync()
+        public Task<List<Produto>> GetAllAsync()
         {
-            try
-            {               
-                return await _context.TB_PRODUTO.Where(x => x.Ativo == 1).ToListAsync();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            throw new NotImplementedException();
         }
 
-        public async Task<List<Produto>> GetAllFilteredAsync(Produto produto)
+        public Task<Produto> GetByIdAsync(int id)
         {
-            try
-            {
-                var produtosBanco = new List<Produto>();
-
-                if (!string.IsNullOrEmpty(produto.Codigo))// Caso recebe código filtra apenas pelo código
-                {
-                    return await _context.TB_PRODUTO.Where(x => x.Ativo == 1 && x.Codigo.Equals(produto.Codigo)).ToListAsync();
-                }
-
-                if (!string.IsNullOrEmpty(produto.Descricao))
-                {
-                    produtosBanco = await _context.TB_PRODUTO.Where(x => x.Ativo == 1 && x.Descricao.Replace(" ", "").ToUpper().Contains(produto.Descricao.Replace(" ", "").ToUpper())).ToListAsync();
-                }
-                
-                if (string.IsNullOrEmpty(produto.Setor))
-                {
-                    produtosBanco = produtosBanco.Where(x => x.Ativo == 1 && x.Setor.Equals(produto.Setor)).ToList();
-                }
-
-                return produtosBanco;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-
-        public async Task<Produto> GetByIdAsync(int id)
-        {
-            try
-            {
-                return await _context.TB_PRODUTO.FindAsync(id);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            throw new NotImplementedException();
         }
 
         public async Task<List<Produto>> GetByKeyAsync(string key)
         {
+            var sql = $"SELECT * FROM tb_produto WHERE codigo = {key}";
             try
             {
-                return await _context.TB_PRODUTO.Where(x => x.Descricao == key).ToListAsync();
+                using var connection = await _connection.GetConnectionAsync();
+                var response = connection.Query<Produto>(sql).ToList();
+                return response;
             }
             catch (Exception)
             {
@@ -128,29 +53,9 @@ namespace Comercio.Data.Repositories
             }
         }
 
-        public async Task<Produto> UpdateAsync(Produto produto)
+        public Task<Produto> UpdateAsync(Produto entity)
         {
-            using (var transaction = await _context.Database.BeginTransactionAsync())
-            {
-                try
-                {
-                    var produtoBanco = await _context.TB_PRODUTO.Where(x => x.Codigo.Equals(produto.Codigo)).FirstAsync();
-                    produtoBanco.Ativo = 1;
-                    produtoBanco.Data_alteracao = DateTime.Now;
-                    produtoBanco.Descricao = produto.Descricao;
-                    produtoBanco.Preco_custo = produto.Preco_custo;
-                    produtoBanco.Preco_venda = produto.Preco_venda;
-                    _context.TB_PRODUTO.Update(produtoBanco);
-                    await _context.SaveChangesAsync();
-                    await transaction.CommitAsync();
-                    return produto;
-                }
-                catch (Exception)
-                {
-                    await transaction.RollbackAsync();
-                    throw;
-                }
-            }
+            throw new NotImplementedException();
         }
     }
 }
