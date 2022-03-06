1caec852357ac2a9698b98ee50104be9c6f3fdfe
RafaelBonotto <rafareisaba@outlook.com>
2022-03-06 01:36:13 -0300

    Update ProdutoQuery.cs

--- SistemaMVC.Comercio/Comercio/Data/Querys/ProdutoQuery.cs
+++ SistemaMVC.Comercio/Comercio/Data/Querys/ProdutoQuery.cs
@@ -2,20 +2,34 @@
 {
     public static class ProdutoQuery
     {
-        public const string SELECT_POR_CODIGO = @"SELECT* FROM 
-                                                  tb_produto WHERE 
-                                                  codigo = @codigo";
+        public const string SELECT_POR_CODIGO = @"SELECT* 
+                                                    FROM 
+                                                    tb_produto  
+                                                    INNER JOIN
+                                                    tb_setor ON tb_setor.id = tb_produto.setor_id
+                                                    WHERE 
+                                                    codigo = @codigo";
 
-        public const string SELECT_POR_DESCRICAO = @"SELECT* FROM 
-                                                        tb_produto WHERE 
-                                                        descricao LIKE CONCAT('%',@descricao,'%')";
+        public const string SELECT_POR_DESCRICAO = @"SELECT * 
+                                                        FROM 
+                                                        tb_produto 
+                                                        INNER JOIN
+                                                        tb_setor ON tb_setor.id = tb_produto.setor_id
+                                                        WHERE 
+                                                        tb_produto.descricao LIKE CONCAT('%',@descricao,'%')";
 
         public const string SELECT_POR_SETOR_ID = @"SELECT* FROM 
-                                                        tb_produto WHERE 
-                                                        setor_id = @setor_id";
+                                                        tb_produto
+                                                        INNER JOIN
+                                                        tb_setor ON tb_setor.id = tb_produto.setor_id
+                                                        WHERE 
+                                                        tb_produto.setor_id = @setor_id";
 
         public const string SELECT_POR_ID = @"SELECT* FROM 
-                                                tb_produto WHERE 
-                                                id = @id";
+                                                tb_produto 
+                                                INNER JOIN
+                                                tb_setor ON tb_setor.id = tb_produto.setor_id
+                                                WHERE 
+                                                tb_produto.id = @produto_id";
     }
 }
