1b327737bf28f624bfcd6de3eb99878f39392a65
RafaelBonotto <rafareisaba@outlook.com>
2022-03-06 01:36:31 -0300

    Create Setor.cs

---
+++ SistemaMVC.Comercio/Comercio/Models/Setor.cs
@@ -0,0 +1,15 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+
+namespace Comercio.Models
+{
+    [Table("tb_setor")]
+    public class Setor
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public sbyte Ativo { get; set; }
+        public DateTime Data_criacao { get; set; }
+        public DateTime Data_alteracao { get; set; }
+    }
+}
