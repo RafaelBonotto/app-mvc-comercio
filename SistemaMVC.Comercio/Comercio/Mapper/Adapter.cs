90bf42fa9220e516f17fe080fd8608af35d272d4
RafaelBonotto <rafareisaba@outlook.com>
2022-03-06 23:01:24 -0300

    Create Adapter.cs

---
+++ SistemaMVC.Comercio/Comercio/Mapper/Adapter.cs
@@ -0,0 +1,19 @@
+﻿using Comercio.Models;
+using System;
+
+namespace Comercio.Mapper
+{
+    public static class Adapter
+    {
+        public static Produto MontaProdutoUpdateRepositorio(Produto produtoRequest, Produto produtoRepositorio)
+        {
+            produtoRepositorio.Descricao = produtoRequest.Descricao;
+            produtoRepositorio.Preco_custo = produtoRequest.Preco_custo;
+            produtoRepositorio.Preco_venda = produtoRequest.Preco_venda;
+            produtoRepositorio.Setor_id = produtoRequest.Setor_id;
+            produtoRepositorio.Ativo = 1;
+            produtoRepositorio.Data_alteracao = DateTime.Now;
+            return produtoRepositorio;
+        }
+    }
+}
