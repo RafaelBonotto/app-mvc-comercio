7f7773c978b5edc92e60883ea3a190d83eeb8cb8
RafaelBonotto <rafareisaba@outlook.com>
2022-03-08 22:41:44 -0300

    Create ProdutoViewModel.cs

---
+++ SistemaMVC.Comercio/Comercio/Models/ProdutoViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Comercio.Models
+{
+    public class ProdutoViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "CÓDIGO")]
+        public string Codigo { get; set; }
+
+        [Display(Name = "PRODUTO")]
+        public string Descricao { get; set; }
+
+        [Display(Name = "CUSTO")]
+        public string Preco_custo { get; set; }
+
+        [Display(Name = "VENDA")]
+        public string Preco_venda { get; set; }
+
+        [Display(Name = "STATUS")]
+        public string Ativo { get; set; } = "Ativo";
+
+        [Display(Name = "SETOR")]
+        public string Setor { get; set; }
+
+        public int Setor_id { get; set; }
+    }
+}
