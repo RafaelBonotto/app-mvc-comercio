ada017c01d470e37aebaea24f410c14f6d72a648
RafaelBonotto <rafareisaba@outlook.com>
2022-03-09 23:42:12 -0300

    Create Produto.cs

---
+++ SistemaMVC.Comercio/Comercio/Entities/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Comercio.Entities
+{
+    [Table("tb_produto")]
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Descricao { get; set; }
+        public double Preco_custo { get; set; }
+        public double Preco_venda { get; set; }
+        public int Setor_id { get; set; }
+        public sbyte Ativo { get; set; }
+        public DateTime Data_criacao { get; set; }
+        public DateTime Data_alteracao { get; set; }
+        public virtual Setor Setor { get; set; }
+    }
+}
