07d288253a839f22bd2b6e8bb6f78d2d35955981
RafaelBonotto <rafareisaba@outlook.com>
2022-03-09 23:42:16 -0300

    Create Setor.cs

---
+++ SistemaMVC.Comercio/Comercio/Entities/Setor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Comercio.Entities
+{
+    [Table("tb_setor")]
+    public class Setor
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public sbyte Ativo { get; set; }
+        public DateTime Data_criacao { get; set; }
+        public DateTime Data_alteracao { get; set; }
+    }
+}
