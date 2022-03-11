86a8db55d6af30b63aa4bebcc1e6c5c937c4f1ba
RafaelBonotto <rafareisaba@outlook.com>
2022-03-10 23:55:33 -0300

    Update ProdutoViewModel.cs

--- SistemaMVC.Comercio/Comercio/Models/ProdutoViewModel.cs
+++ SistemaMVC.Comercio/Comercio/Models/ProdutoViewModel.cs
@@ -7,21 +7,26 @@ namespace Comercio.Models
         public int Id { get; set; }
 
         [Display(Name = "CÓDIGO")]
+        [Required(ErrorMessage = ("O campo código é obrigatório"))]
+        [MinLength(6, ErrorMessage = "O campo código deve ter no mínimo 6 dígitos")]
         public string Codigo { get; set; }
 
         [Display(Name = "PRODUTO")]
+        [Required(ErrorMessage = ("O campo descrição é obrigatório"))]
         public string Descricao { get; set; }
 
         [Display(Name = "CUSTO")]
+        [Required(ErrorMessage = ("O preço de custo do produto é obrigatório"))]
         public string Preco_custo { get; set; }
 
         [Display(Name = "VENDA")]
+        [Required(ErrorMessage = ("O preço de venda do produto é obrigatório"))]
         public string Preco_venda { get; set; }
 
         [Display(Name = "STATUS")]
-        public string Ativo { get; set; } = "Ativo";
+        public string Ativo { get; set; } 
 
-        [Display(Name = "SETOR")]
+        [Display(Name = "SETOR")]        
         public string Setor { get; set; }
 
         public int Setor_id { get; set; }
