8f017e634e8e331b92141afa309a79b5d78535cb
RafaelBonotto <rafareisaba@outlook.com>
2022-03-14 17:32:30 -0300

    Update ProdutoQuery.cs

--- SistemaMVC.Comercio/Comercio/Data/Querys/ProdutoQuery.cs
+++ SistemaMVC.Comercio/Comercio/Data/Querys/ProdutoQuery.cs
@@ -6,35 +6,41 @@
                                                     FROM 
                                                     tb_produto  
                                                     INNER JOIN
-                                                    tb_setor ON tb_setor.id = tb_produto.setor_id
+                                                    tb_setor 
+                                                    ON tb_setor.id = tb_produto.setor_id
                                                     WHERE 
                                                     codigo = @codigo";
 
-        public const string SELECT_POR_DESCRICAO = @"SELECT * 
+        public const string SELECT_POR_DESCRICAO = @"SELECT* 
                                                         FROM 
                                                         tb_produto 
                                                         INNER JOIN
-                                                        tb_setor ON tb_setor.id = tb_produto.setor_id
+                                                        tb_setor        
+                                                        ON tb_setor.id = tb_produto.setor_id
                                                         WHERE 
                                                         tb_produto.ativo = 1
                                                         AND
                                                         tb_produto.descricao LIKE CONCAT('%',@descricao,'%')
                                                         ORDER BY tb_produto.descricao";
 
-        public const string SELECT_POR_SETOR_ID = @"SELECT* FROM 
+        public const string SELECT_POR_SETOR_ID = @"SELECT* 
+                                                        FROM 
                                                         tb_produto
                                                         INNER JOIN
-                                                        tb_setor ON tb_setor.id = tb_produto.setor_id
+                                                        tb_setor 
+                                                        ON tb_setor.id = tb_produto.setor_id
                                                         WHERE 
                                                         tb_produto.ativo = 1
                                                         AND
                                                         tb_produto.setor_id = @setor_id
                                                         ORDER BY tb_produto.descricao";
 
-        public const string SELECT_POR_ID = @"SELECT* FROM 
+        public const string SELECT_POR_ID = @"SELECT* 
+                                                FROM 
                                                 tb_produto 
                                                 INNER JOIN
-                                                tb_setor ON tb_setor.id = tb_produto.setor_id
+                                                tb_setor 
+                                                ON tb_setor.id = tb_produto.setor_id
                                                 WHERE 
                                                 tb_produto.id = @produto_id";
     }
