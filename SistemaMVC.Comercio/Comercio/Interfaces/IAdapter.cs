40238d227324ec5bc5a0f5e5e740376d2f843759
RafaelBonotto <rafareisaba@outlook.com>
2022-03-15 11:49:03 -0300

    Create IAdapter.cs

---
+++ SistemaMVC.Comercio/Comercio/Interfaces/IAdapter.cs
@@ -0,0 +1,12 @@
+﻿using Comercio.Entities;
+using Comercio.Models;
+
+namespace Comercio.Interfaces
+{
+    public interface IAdapter
+    {
+        Produto MontaProdutoUpdateRepositorio(ProdutoViewModel produtoViewModel, Produto produtoRepositorio);
+        Produto MontaProdutoInsertRepositorio(ProdutoViewModel produtoViewModel);
+        ProdutoViewModel MontaProdutoViewModel(Produto produto);
+    }
+}
