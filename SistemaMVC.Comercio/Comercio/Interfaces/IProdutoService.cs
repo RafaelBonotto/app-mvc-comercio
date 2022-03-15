ff82cac5da57eab56c4ad58321f8408f8da434fb
RafaelBonotto <rafareisaba@outlook.com>
2022-03-15 11:49:08 -0300

    Update IProdutoService.cs

--- SistemaMVC.Comercio/Comercio/Interfaces/IProdutoService.cs
+++ SistemaMVC.Comercio/Comercio/Interfaces/IProdutoService.cs
@@ -12,7 +12,7 @@ namespace Comercio.Interfaces
         Task<List<Produto>> FiltrarPorDescricao(string descricao);
         Task<List<Produto>> FiltrarPorSetor(int setor_id);
         Task<Produto> DetalhesProduto(int id);        
-        Task<Produto> InserirProduto(Produto produto);
+        Task<Produto> InserirProduto(ProdutoViewModel produto);
         Task<Produto> AtualizarProduto(ProdutoViewModel produto);
         Task<bool> ExcluirProduto(int produtoId);
         Task<List<Fornecedor>> ObterFornecedor(int produtoId);
