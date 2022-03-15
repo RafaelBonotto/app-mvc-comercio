da1adb002d7d584101d868af8d35a0d0f327ef52
RafaelBonotto <rafareisaba@outlook.com>
2022-03-15 15:40:00 -0300

    Update Adapter.cs

--- SistemaMVC.Comercio/Comercio/Mapper/Adapter.cs
+++ SistemaMVC.Comercio/Comercio/Mapper/Adapter.cs
@@ -1,4 +1,5 @@
 ﻿using Comercio.Entities;
+using Comercio.Enum;
 using Comercio.Interfaces;
 using Comercio.Models;
 using System;
@@ -12,7 +13,7 @@ namespace Comercio.Mapper
             produtoRepositorio.Descricao = produtoViewModel.Descricao;
             produtoRepositorio.Preco_custo = double.Parse(produtoViewModel.Preco_custo.Replace(".", ","));
             produtoRepositorio.Preco_venda = double.Parse(produtoViewModel.Preco_venda.Replace(".", ","));
-            produtoRepositorio.Setor_id = produtoViewModel.Setor_id;
+            produtoRepositorio.Setor_id = produtoViewModel.Setor.GetHashCode();
             produtoRepositorio.Ativo = 1;
             produtoRepositorio.Data_alteracao = DateTime.Now;
             return produtoRepositorio;
@@ -25,7 +26,7 @@ namespace Comercio.Mapper
                 Descricao = produtoViewModel.Descricao,
                 Preco_custo = double.Parse(produtoViewModel.Preco_custo.Replace(".", ",")),
                 Preco_venda = double.Parse(produtoViewModel.Preco_venda.Replace(".", ",")),
-                Setor_id = produtoViewModel.Setor_id,
+                Setor_id = produtoViewModel.Setor.GetHashCode(),
                 Ativo = 1,
                 Data_criacao = DateTime.Now,
                 Data_alteracao = DateTime.Now
@@ -34,7 +35,7 @@ namespace Comercio.Mapper
 
         public ProdutoViewModel MontaProdutoViewModel(Produto produto)
         {
-            return new ProdutoViewModel()
+            var aux = new ProdutoViewModel()
             {
                 Id = produto.Id,
                 Codigo = produto.Codigo,
@@ -42,8 +43,9 @@ namespace Comercio.Mapper
                 Preco_custo = produto.Preco_custo.ToString("N2"),
                 Preco_venda = produto.Preco_venda.ToString("N2"),
                 Ativo = produto.Ativo == 0 ? "Inativo " : "Ativo",
-                Setor = produto.Setor.Descricao
+                Setor = (Setores)produto.Setor_id
             };
+            return aux;
         }
     }
 }
