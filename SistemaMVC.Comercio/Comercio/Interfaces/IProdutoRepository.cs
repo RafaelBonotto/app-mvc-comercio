ed236dae69f81db7abe26ac94a7f384274ff7ff8
RafaelBonotto <rafareisaba@outlook.com>
2022-03-17 18:26:51 -0300

    Update IProdutoRepository.cs

--- SistemaMVC.Comercio/Comercio/Interfaces/IProdutoRepository.cs
+++ SistemaMVC.Comercio/Comercio/Interfaces/IProdutoRepository.cs
@@ -7,7 +7,7 @@ namespace Comercio.Interfaces
     public interface IProdutoRepository
     {
         Task<List<Produto>> FiltrarPorDescricao(string descricao);
-        Task<List<Produto>> FiltrarPorSetor(int setor_id);
-        Task<List<Setor>> ListarSetoresBanco();
+        Task<List<Produto>> FiltrarPorSetor(string setor);
+        Task<List<Setor>> ObterSetores();
     }
 }
