012870d9fb28ba9c6d7cc32b7b15765d4a1d9eda
RafaelBonotto <rafareisaba@outlook.com>
2022-03-18 12:18:48 -0300

    Update ErrorViewModel.cs

--- SistemaMVC.Comercio/Comercio/Models/ErrorViewModel.cs
+++ SistemaMVC.Comercio/Comercio/Models/ErrorViewModel.cs
@@ -4,8 +4,15 @@ namespace Comercio.Models
 {
     public class ErrorViewModel
     {
+        public ErrorViewModel(){}
+        public ErrorViewModel(string msg, int statusCode)
+        {
+            Mensagem = msg;
+            StatusCode = statusCode;
+        }
         public string RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string Mensagem { get; set; }
+        public int StatusCode { get; set; }
     }
 }
