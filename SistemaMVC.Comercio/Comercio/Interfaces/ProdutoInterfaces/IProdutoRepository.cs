77cb91bf84e777943bcb85bbbcdbe3de7cca199a
RafaelBonotto <rafareisaba@outlook.com>
2022-03-18 11:10:56 -0300

    Create IProdutoRepository.cs

---
+++ SistemaMVC.Comercio/Comercio/Interfaces/ProdutoInterfaces/IProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Comercio.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Comercio.Interfaces.ProdutoInterfaces
+{
+    public interface IProdutoRepository
+    {
+        Task<List<Produto>> FiltrarPorDescricao(string descricao);
+        Task<List<Produto>> FiltrarPorSetor(string setor);
+        Task<List<Setor>> ObterSetores();
+        Task<int> ObterSetorId(string setor);
+    }
+}
