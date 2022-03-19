c2072e39a48d64c08f4b26c45e02a2a3e4bac0a7
RafaelBonotto <rafareisaba@outlook.com>
2022-03-19 00:15:29 -0300

    Create CodigoInvalidoException.cs

---
+++ SistemaMVC.Comercio/Comercio/Exceptions/Produto/CodigoInvalidoException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Comercio.Exceptions.Produto
+{
+    public class CodigoInvalidoException : Exception
+    {
+        public string Mensagem { get; set; }
+
+        public CodigoInvalidoException()
+        {
+            Mensagem = "Código inválido!!!";
+        }
+        public CodigoInvalidoException(string mensagem) : base(mensagem)
+        {
+           
+        }
+    }
+}
