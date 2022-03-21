c585033c81145e77a61e65fd5a434702fb8d0c93
RafaelBonotto <rafareisaba@outlook.com>
2022-03-21 14:35:32 -0300

    Update ErrorViewModel.cs

--- SistemaMVC.Comercio/Comercio/Models/ErrorViewModel.cs
+++ SistemaMVC.Comercio/Comercio/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Comercio.Models
 {
     public class ErrorViewModel
@@ -10,5 +8,53 @@ namespace Comercio.Models
         public string RequestId { get; set; }
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
         public string Mensagem { get; set; }
+
+        public ErrorViewModel ErroAoTentarCarregarPagina()
+        {
+            this.Mensagem = "Algo deu errado ao tentar carregar essa página.";
+            return this;
+        }
+
+        public ErrorViewModel ErroFiltroNaoEncontrado()
+        {
+            this.Mensagem = "Não foram encontrados itens para esse filtro.";
+            return this;
+        }
+
+        public ErrorViewModel ErroAoCarregarDetalhes()
+        {
+            this.Mensagem = "Algo deu errado ao tentar carregar a página de detalhes.";
+            return this;
+        }
+
+        public ErrorViewModel ErroDeValidacao()
+        {
+            this.Mensagem = "Erro de validação, verifique se os valores dos campos de entrada são válidos.";
+            return this;
+        }
+
+        public ErrorViewModel ProdutoErroCodigoInvalido()
+        {
+            this.Mensagem = "Não foi possível inserir ou atualizar o produto, foi encontrado um produto ativo com o mesmo código.";
+            return this;
+        }
+
+        public ErrorViewModel ProdutoErroAoTentarInserir()
+        {
+            this.Mensagem = "Não foi possível inserir o produto.";
+            return this;
+        }
+
+        public ErrorViewModel ProdutoErroAoTentarAtualizar()
+        {
+            this.Mensagem = "Não foi possível atualizar o produto.";
+            return this;
+        }
+
+        public ErrorViewModel ProdutoErroAoTentarExcluir()
+        {
+            this.Mensagem = "Não foi possível excluir o produto.";
+            return this;
+        }
     }
 }
