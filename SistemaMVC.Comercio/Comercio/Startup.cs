680ccd8f3d7e1347bbc8dfe8d2955d15df85857f
RafaelBonotto <rafareisaba@outlook.com>
2022-03-21 14:35:51 -0300

    Update Startup.cs

--- SistemaMVC.Comercio/Comercio/Startup.cs
+++ SistemaMVC.Comercio/Comercio/Startup.cs
@@ -1,5 +1,6 @@
 using Comercio.Data.ConnectionManager;
 using Comercio.Data.Repositories;
+using Comercio.Data.Repositories.Produtos;
 using Comercio.Entities;
 using Comercio.Interfaces;
 using Comercio.Interfaces.Base;
