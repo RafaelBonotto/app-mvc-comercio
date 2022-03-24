0536b962da6704c9abcd25d5e9ad8ee3bc859a05
RafaelBonotto <rafareisaba@outlook.com>
2022-03-23 23:50:54 -0300

    Create ISetorService.cs

---
+++ SistemaMVC.Comercio/Comercio/Interfaces/SetorInterfaces/ISetorService.cs
@@ -0,0 +1,11 @@
+﻿using Comercio.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Comercio.Interfaces.SetorInterfaces
+{
+    public interface ISetorService
+    {
+        Task<IEnumerable<Setor>> ListarSetores();
+    }
+}
