b9839aed0d2e31d4760e377c7299d923ccf2f357
RafaelBonotto <rafareisaba@outlook.com>
2022-03-23 23:50:45 -0300

    Create ISetorAdapter.cs

---
+++ SistemaMVC.Comercio/Comercio/Interfaces/SetorInterfaces/ISetorAdapter.cs
@@ -0,0 +1,11 @@
+﻿using Comercio.Entities;
+using Comercio.Models;
+using System.Collections.Generic;
+
+namespace Comercio.Interfaces.SetorInterfaces
+{
+    public interface ISetorAdapter
+    {
+        IEnumerable<SetorViewModel> MontaSetorViewModel(IEnumerable<Setor> setoresBanco);
+    }
+}
