0a94624328cedaac706a128c3d28959cd03b6c4c
RafaelBonotto <rafareisaba@outlook.com>
2022-03-23 23:51:00 -0300

    Create SetorAdapter.cs

---
+++ SistemaMVC.Comercio/Comercio/Mapper/SetorAdapter.cs
@@ -0,0 +1,22 @@
+﻿using Comercio.Entities;
+using Comercio.Interfaces.SetorInterfaces;
+using Comercio.Models;
+using System.Collections.Generic;
+
+namespace Comercio.Mapper
+{
+    public class SetorAdapter : ISetorAdapter
+    {
+        public IEnumerable<SetorViewModel> MontaSetorViewModel(IEnumerable<Setor> setoresBanco)
+        {
+            List<SetorViewModel> ret = new();
+            foreach (var setor in setoresBanco)
+                ret.Add(new SetorViewModel()
+                {
+                    Id = setor.Id,
+                    Descricao = setor.Descricao.ToUpper()
+                });
+            return ret;
+        }
+    }
+}
