06e8c1f17871d7ee98e3b972b0d335a5afd4378a
RafaelBonotto <rafareisaba@outlook.com>
2022-03-23 23:50:50 -0300

    Create ISetorRepository.cs

---
+++ SistemaMVC.Comercio/Comercio/Interfaces/SetorInterfaces/ISetorRepository.cs
@@ -0,0 +1,11 @@
+﻿using Comercio.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Comercio.Interfaces.SetorInterfaces
+{
+    public interface ISetorRepository
+    {
+        Task<IEnumerable<Setor>> ListarSetores();
+    }
+}
