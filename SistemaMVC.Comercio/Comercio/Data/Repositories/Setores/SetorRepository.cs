67aa472fd12ece8dbd7c7e184be794a2830d4937
RafaelBonotto <rafareisaba@outlook.com>
2022-03-23 23:50:31 -0300

    Create SetorRepository.cs

---
+++ SistemaMVC.Comercio/Comercio/Data/Repositories/Setores/SetorRepository.cs
@@ -0,0 +1,25 @@
+﻿using Comercio.Data.ConnectionManager;
+using Comercio.Entities;
+using Comercio.Interfaces.SetorInterfaces;
+using Dapper;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Comercio.Data.Repositories.Setores
+{
+    public class SetorRepository : ISetorRepository
+    {
+        private readonly IMySqlConnectionManager _connection;
+
+        public SetorRepository(IMySqlConnectionManager connection)
+        {
+            _connection = connection;
+        }
+        public async Task<IEnumerable<Setor>> ListarSetores()
+        {
+            var sql = "SELECT * FROM tb_setor WHERE tb_setor.ativo = 1";
+            using var connection = await _connection.GetConnectionAsync();
+            return await connection.QueryAsync<Setor>(sql);
+        }
+    }
+}
