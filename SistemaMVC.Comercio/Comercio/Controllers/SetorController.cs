5f545cf7cf09d12bdac63d10920b32829a103ebc
RafaelBonotto <rafareisaba@outlook.com>
2022-03-23 23:50:16 -0300

    Create SetorController.cs

---
+++ SistemaMVC.Comercio/Comercio/Controllers/SetorController.cs
@@ -0,0 +1,31 @@
+﻿using Comercio.Interfaces.SetorInterfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Comercio.Controllers
+{
+    public class SetorController : Controller
+    {
+        private readonly ISetorService _service;
+        private readonly ISetorAdapter _mapper;
+
+        public SetorController(ISetorService service, ISetorAdapter mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var setoresBanco =  await _service.ListarSetores();
+                var setoresView = _mapper.MontaSetorViewModel(setoresBanco);
+                return View(setoresView);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
