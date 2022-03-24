2961b24f085c71141eb1f2f00a6e2de82a2f20d4
RafaelBonotto <rafareisaba@outlook.com>
2022-03-23 23:51:04 -0300

    Create SetorViewModel.cs

---
+++ SistemaMVC.Comercio/Comercio/Models/SetorViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Comercio.Entities;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Comercio.Models
+{
+    public class SetorViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "SETOR")]
+        [MaxLength(50)]
+        [Required(ErrorMessage = ("O campo descrição é obrigatório"), AllowEmptyStrings = false)]
+        public string Descricao { get; set; }
+
+        [Display(Name = "STATUS")]
+        public string Ativo { get; set; }
+
+        public virtual List<Produto> Produtos { get; set; }
+    }
+}
