2f7622e4ddd8160733c86e58d5584503b982e64a
RafaelBonotto <rafareisaba@outlook.com>
2022-03-25 23:20:35 -0300

    Create SetorService.cs

---
+++ SistemaMVC.Comercio/Comercio/Services/SetorService.cs
@@ -0,0 +1,29 @@
+﻿using Comercio.Entities;
+using Comercio.Interfaces.SetorInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Comercio.Services
+{
+    public class SetorService : ISetorService
+    {
+        private readonly ISetorRepository _setorRepository;
+
+        public SetorService(ISetorRepository repository)
+        {
+            _setorRepository = repository;
+        }
+        public async Task<IEnumerable<Setor>> ListarSetores()
+        {
+            try
+            {
+                return await _setorRepository.ListarSetores();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
