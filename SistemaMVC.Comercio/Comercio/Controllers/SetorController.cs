9181a79d05711dc9d69e4979edb6e1d366cd502a
RafaelBonotto <rafareisaba@outlook.com>
2022-04-02 00:56:38 -0300

    Update SetorController.cs

--- SistemaMVC.Comercio/Comercio/Controllers/SetorController.cs
+++ SistemaMVC.Comercio/Comercio/Controllers/SetorController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 namespace Comercio.Controllers
 {
+    [Route("[controller]")]
     public class SetorController : Controller
     {
         private readonly ISetorService _service;
@@ -14,20 +15,6 @@ namespace Comercio.Controllers
         {
             _service = service;
             _mapper = mapper;
-        }
-
-        public async Task<IActionResult> Editar(int id)
-        {
-            try
-            {
-                var setorBanco = await _service.ObterSetor(id);
-                var setorViewModel = _mapper.MontaSetorViewModel(setorBanco);
-                return View("Editar", setorViewModel);
-            }
-            catch (System.Exception)
-            {
-                throw;
-            }
         }
 
         public async Task<IActionResult> Index()
@@ -41,6 +28,65 @@ namespace Comercio.Controllers
             catch (System.Exception)
             {
                 throw;
+            }
+        }
+
+        [Route("editar/{id}")]
+        public async Task<IActionResult> Editar(int id)
+        {
+            try
+            {
+                var setorBanco = await _service.ObterSetor(id);
+                var setorViewModel = _mapper.MontaSetorViewModel(setorBanco);
+                return View("Editar", setorViewModel);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+        [HttpPost]
+        [Route("atualizar/{id}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Atualizar([Bind("Id, Descricao")] SetorViewModel setor)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var setorResponse = await _service.AtualizarSetor(setor);
+                    if (setorResponse is null)
+                        return View("Error", new ErrorViewModel().SetorErroAoTentarAtualizar()); 
+
+                    var setoresViewModel = _mapper.MontaListaSetorViewModel(await _service.ListarSetores());
+                    return View("Index", setoresViewModel);
+                }
+                catch (System.Exception)
+                {
+                    return View("Error", new ErrorViewModel().ErroAoTentarCarregarPagina());
+                }
+            }
+            else
+            {
+                return View("Error", new ErrorViewModel().ErroDeValidacao());
+            }
+        }
+
+        [Route("excluir/{id}")]
+        public async Task<IActionResult> Excluir(int id)
+        {
+            try
+            {
+                var delete = await _service.ExcluirSetor(id);
+                if (!delete)
+                    return View("Error", new ErrorViewModel().SetorErroAoTentarExcluir());
+
+                var setoresViewModel = _mapper.MontaListaSetorViewModel(await _service.ListarSetores());
+                return View("Index", setoresViewModel);
+            }
+            catch (System.Exception)
+            {
+                return View("Error", new ErrorViewModel().ErroAoTentarCarregarPagina());
             }
         }
     }
