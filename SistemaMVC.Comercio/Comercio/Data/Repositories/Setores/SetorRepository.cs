c43bfe6d55eec1c4bfb1783e25a9f5a2cdc33fc7
RafaelBonotto <rafareisaba@outlook.com>
2022-04-01 23:41:19 -0300

    Update SetorRepository.cs

--- SistemaMVC.Comercio/Comercio/Data/Repositories/Setores/SetorRepository.cs
+++ SistemaMVC.Comercio/Comercio/Data/Repositories/Setores/SetorRepository.cs
@@ -1,13 +1,14 @@
 ﻿using Comercio.Data.ConnectionManager;
 using Comercio.Entities;
-using Comercio.Interfaces.SetorInterfaces;
-using Dapper;
+using Comercio.Interfaces.Base;
+using Dapper.Contrib.Extensions;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Comercio.Data.Repositories.Setores
 {
-    public class SetorRepository : ISetorRepository
+    public class SetorRepository : IRepositoryBase<Setor>
     {
         private readonly IMySqlConnectionManager _connection;
 
@@ -15,11 +16,65 @@ namespace Comercio.Data.Repositories.Setores
         {
             _connection = connection;
         }
-        public async Task<IEnumerable<Setor>> ListarSetores()
+
+        public Task<Setor> AddAsync(Setor entity)
         {
-            var sql = "SELECT * FROM tb_setor WHERE tb_setor.ativo = 1";
-            using var connection = await _connection.GetConnectionAsync();
-            return await connection.QueryAsync<Setor>(sql);
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Setor> DeleteAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<List<Setor>> GetAllAsync()
+        {
+            try
+            {
+                using var connection = await _connection.GetConnectionAsync();
+                //return (await connection.QueryAsync<Setor>(SetorQuerys.OBTER_SETORES)).ToList();
+                return (await connection.GetAllAsync<Setor>()).ToList();
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<Setor> GetByIdAsync(int id)
+        {
+            try
+            {
+                using var connection = await _connection.GetConnectionAsync();
+                //return await connection.QueryFirstOrDefaultAsync<Setor>(SetorQuerys.OBTER_SETORES, new { id });
+                return await connection.GetAsync<Setor>(id);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+
+        public Task<List<Setor>> GetByKeyAsync(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Setor> UpdateAsync(Setor setor)
+        {
+            try
+            {
+                using var connection = await _connection.GetConnectionAsync();
+                var response = await connection.UpdateAsync<Setor>(setor);
+                if (!response)
+                    return null;
+                return await this.GetByIdAsync(setor.Id);
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
