6343777f1bcbe53e7d3d025e13080e601bbde80f
RafaelBonotto <rafareisaba@outlook.com>
2022-04-01 23:40:53 -0300

    Create ProdutoQuerys.cs

---
+++ SistemaMVC.Comercio/Comercio/Data/Querys/ProdutoQuerys.cs
@@ -0,0 +1,57 @@
+﻿namespace Comercio.Data.Querys
+{
+    public static class ProdutoQuerys
+    {
+        public const string SELECT_POR_CODIGO = @"SELECT* 
+                                                    FROM 
+                                                    tb_produto  
+                                                    INNER JOIN
+                                                    tb_setor 
+                                                    ON tb_setor.id = tb_produto.setor_id
+                                                    WHERE 
+                                                    tb_produto.codigo = @Codigo";
+
+        public const string SELECT_POR_DESCRICAO = @"SELECT* 
+                                                        FROM 
+                                                        tb_produto 
+                                                        INNER JOIN
+                                                        tb_setor        
+                                                        ON tb_setor.id = tb_produto.setor_id
+                                                        WHERE 
+                                                        tb_produto.ativo = 1
+                                                        AND
+                                                        tb_produto.descricao LIKE CONCAT('%',@descricao,'%')
+                                                        ORDER BY tb_produto.descricao";
+
+        public const string SELECT_POR_SETOR = @"SELECT* 
+                                                    FROM 
+                                                    tb_produto
+                                                    INNER JOIN
+                                                    tb_setor 
+                                                    ON tb_setor.id = tb_produto.setor_id
+                                                    WHERE 
+                                                    tb_produto.ativo = 1
+                                                    AND
+                                                    tb_setor.descricao = @setor
+                                                    ORDER BY tb_produto.descricao";
+
+        public const string SELECT_POR_ID = @"SELECT* 
+                                                FROM 
+                                                tb_produto 
+                                                INNER JOIN
+                                                tb_setor 
+                                                ON tb_setor.id = tb_produto.setor_id
+                                                WHERE 
+                                                tb_produto.id = @produto_id";
+
+        public const string SELECT_LISTAR_SETORES = @"SELECT * 
+                                                        FROM tb_setor 
+                                                        WHERE tb_setor.ativo = 1";
+
+        public const string SELECT_ID_SETOR = @"SELECT id 
+                                                    FROM
+                                                    tb_setor
+                                                    WHERE
+                                                    tb_setor.descricao LIKE CONCAT('%',@descricao,'%')";
+    }
+}
