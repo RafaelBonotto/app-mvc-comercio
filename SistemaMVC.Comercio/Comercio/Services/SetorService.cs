fbc91132b5128c93b19befae670d4bfb83729d93
RafaelBonotto <rafareisaba@outlook.com>
2022-04-02 00:57:13 -0300

    Update SetorService.cs

--- SistemaMVC.Comercio/Comercio/Services/SetorService.cs
+++ SistemaMVC.Comercio/Comercio/Services/SetorService.cs
@@ -1,6 +1,7 @@
 ﻿using Comercio.Entities;
 using Comercio.Interfaces.Base;
 using Comercio.Interfaces.SetorInterfaces;
+using Comercio.Models;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -10,10 +11,12 @@ namespace Comercio.Services
     public class SetorService : ISetorService
     {
         private readonly IRepositoryBase<Setor> _repositoryBase;
+        private readonly ISetorAdapter _mapper;
 
-        public SetorService(IRepositoryBase<Setor> repository)
+        public SetorService(IRepositoryBase<Setor> repository, ISetorAdapter mapper)
         {
             _repositoryBase = repository;
+            _mapper = mapper;
         }
 
         public async Task<IEnumerable<Setor>> ListarSetores()
@@ -40,13 +43,28 @@ namespace Comercio.Services
             }
         }
 
-        public async Task<Setor> AtualizarSetor(Setor setor)
+        public async Task<Setor> AtualizarSetor(SetorViewModel setor)
         {
             try
             {
                 var setorBanco = await _repositoryBase.GetByIdAsync(setor.Id);
-                setorBanco.Descricao = setor.Descricao;
+                setorBanco.Descricao = setor.Descricao.ToUpper();
                 return await _repositoryBase.UpdateAsync(setorBanco);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> ExcluirSetor(int id)
+        {
+            try
+            {
+                Setor response = await _repositoryBase.DeleteAsync(id);
+                if (response is null)
+                    return false;
+                return true;
             }
             catch (Exception)
             {
