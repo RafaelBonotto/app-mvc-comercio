cd2da7bd3bd51fad6f186fc5b2e6c15715ff53a1
RafaelBonotto <rafareisaba@outlook.com>
2022-04-03 01:05:03 -0300

    Create DescricaoInvalidaException.cs

---
+++ SistemaMVC.Comercio/Comercio/Exceptions/Setor/DescricaoInvalidaException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Comercio.Exceptions.Setor
+{
+    public class DescricaoInvalidaException : Exception
+    {
+        public DescricaoInvalidaException()
+        {
+        }
+        public DescricaoInvalidaException(string mensagem) : base(mensagem)
+        {
+        }
+    }
+}
