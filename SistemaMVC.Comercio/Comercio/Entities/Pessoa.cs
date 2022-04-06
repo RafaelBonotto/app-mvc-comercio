92e909af662dff41e65d7a4b208ad10c4f7d0fcc
RafaelBonotto <rafareisaba@outlook.com>
2022-04-05 23:30:57 -0300

    Create Pessoa.cs

---
+++ SistemaMVC.Comercio/Comercio/Entities/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Comercio.Entities
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string DataNasc { get; set; }
+        public string Genero { get; set; }
+        public List<Telefone> Telefones { get; set; }
+        public List<Endereco> Endereco { get; set; }
+    }
+}
