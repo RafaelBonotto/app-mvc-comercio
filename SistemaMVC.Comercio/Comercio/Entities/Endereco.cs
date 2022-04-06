72d26d02158ef67d2ac9aeff7c33051ed8b0438a
RafaelBonotto <rafareisaba@outlook.com>
2022-04-05 23:30:49 -0300

    Create Endereco.cs

---
+++ SistemaMVC.Comercio/Comercio/Entities/Endereco.cs
@@ -0,0 +1,16 @@
+﻿using Comercio.Enums;
+
+namespace Comercio.Entities
+{
+    public class Endereco
+    {
+        public TipoEndereco Tipo { get; set; }
+        public string Logradouro { get; set; }
+        public string NumeroLogradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Cep { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+    }
+}
