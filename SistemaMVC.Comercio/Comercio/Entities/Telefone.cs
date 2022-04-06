bd9e923eafefb02c2b0975474f47128e5c9770f5
RafaelBonotto <rafareisaba@outlook.com>
2022-04-05 23:31:02 -0300

    Create Telefone.cs

---
+++ SistemaMVC.Comercio/Comercio/Entities/Telefone.cs
@@ -0,0 +1,11 @@
+﻿using Comercio.Enums;
+
+namespace Comercio.Entities
+{
+    public class Telefone
+    {
+        public TipoTelefone Tipo { get; set; }
+        public string Ddd { get; set; }
+        public string Numero { get; set; }
+    }
+}
