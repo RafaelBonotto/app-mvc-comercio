bfc76ab8f466cdef95e906b7375319018b2157c9
RafaelBonotto <rafareisaba@outlook.com>
2022-04-08 16:58:15 -0300

    Update Fornecedor.cs

--- SistemaMVC.Comercio/Comercio/Entities/Fornecedor.cs
+++ SistemaMVC.Comercio/Comercio/Entities/Fornecedor.cs
@@ -7,16 +7,10 @@ namespace Comercio.Entities
     public class Fornecedor
     {
         public int Id { get; set; }
-
-        [Display(Name = "Fornecedor")]
         public string Nome_empresa { get; set; }
-
-        [Display(Name = "Telefone")]
-        public List<Telefone> Telefones { get; set; }
-
-        [Display(Name = "Vendedor")]
-        public List<Pessoa> Vendedores { get; set; }
-
+        public string Telefone_empresa { get; set; }
+        public string Nome_vendedor{ get; set; }
+        public string Telefone_vendedor { get; set; }
         public sbyte Ativo { get; set; }
         public DateTime Data_criacao { get; set; }
         public DateTime Data_alteracao { get; set; }
