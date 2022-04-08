4c384f411ec8dac5132f717819f8573cc50d77fa
RafaelBonotto <rafareisaba@outlook.com>
2022-04-08 16:58:32 -0300

    Create FornecedorViewModel.cs

---
+++ SistemaMVC.Comercio/Comercio/Models/FornecedorViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Comercio.Entities;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Comercio.Models
+{
+    public class FornecedorViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Fornecedor")]
+        public string Nome_empresa { get; set; }
+
+        [Display(Name = "Telefone")]
+        public List<Telefone> Telefones { get; set; }
+
+        [Display(Name = "Vendedor")]
+        public List<Pessoa> Vendedores { get; set; }
+    }
+}
