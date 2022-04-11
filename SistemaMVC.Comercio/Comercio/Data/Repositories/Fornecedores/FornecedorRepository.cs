a7974a81402546d658965df913af38047aff9290
RafaelBonotto <rafareisaba@outlook.com>
2022-04-10 21:01:48 -0300

    Create FornecedorRepository.cs

---
+++ SistemaMVC.Comercio/Comercio/Data/Repositories/Fornecedores/FornecedorRepository.cs
@@ -0,0 +1,41 @@
+﻿using Comercio.Entities;
+using Comercio.Interfaces.Base;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Comercio.Data.Repositories.Fornecedores
+{
+    public class FornecedorRepository : IRepositoryBase<Fornecedor>
+    {
+        public Task<Fornecedor> AddAsync(Fornecedor entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Fornecedor> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Fornecedor>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Fornecedor> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Fornecedor>> GetByKeyAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Fornecedor> UpdateAsync(Fornecedor entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
