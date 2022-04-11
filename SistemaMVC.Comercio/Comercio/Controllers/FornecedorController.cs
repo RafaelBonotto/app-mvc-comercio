3f02c424d4169d9036fd6871eca71b840819ad0b
RafaelBonotto <rafareisaba@outlook.com>
2022-04-10 21:01:43 -0300

    Create FornecedorController.cs

---
+++ SistemaMVC.Comercio/Comercio/Controllers/FornecedorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Comercio.Controllers
+{
+    public class FornecedorController : Controller
+    {
+        private readonly IFornecedorService _service;
+        private readonly IFornecedorAdapter _mapper;
+
+        public FornecedorController(IFornecedorService fornecedorService, IFornecedorAdapter adaper)
+        {
+            _fornecedorService = fornecedorService;
+            _mapper = adaper;
+        }
+
+        public IActionResult Index() => View();
+    }
+}
