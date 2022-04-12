7eb39809c98f649b869d3486ed3e736efb51d23c
RafaelBonotto <rafareisaba@outlook.com>
2022-04-12 17:04:26 -0300

    Update FornecedorService.cs

--- SistemaMVC.Comercio/Comercio/Services/FornecedorService.cs
+++ SistemaMVC.Comercio/Comercio/Services/FornecedorService.cs
@@ -1,9 +1,20 @@
-﻿using Comercio.Interfaces.FornecedorInterfaces;
+﻿using Comercio.Entities;
+using Comercio.Interfaces.FornecedorInterfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Comercio.Services
 {
     public class FornecedorService : IFornecedorService
     {
+        public Task<List<Fornecedor>> FiltrarPorSetor(string setor)
+        {
+            throw new System.NotImplementedException();
+        }
 
+        public Task<List<Fornecedor>> ListarFornecedores()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
