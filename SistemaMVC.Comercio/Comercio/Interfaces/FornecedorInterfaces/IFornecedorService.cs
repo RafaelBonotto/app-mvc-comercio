714e83b056fe7ca5ad48b609270c3befaf7cb04b
RafaelBonotto <rafareisaba@outlook.com>
2022-04-19 18:06:28 -0300

    Update IFornecedorService.cs

--- SistemaMVC.Comercio/Comercio/Interfaces/FornecedorInterfaces/IFornecedorService.cs
+++ SistemaMVC.Comercio/Comercio/Interfaces/FornecedorInterfaces/IFornecedorService.cs
@@ -6,6 +6,7 @@ namespace Comercio.Interfaces.FornecedorInterfaces
 {
     public interface IFornecedorService
     {
+        Task<Fornecedor> InserirFornecedor();
         Task<List<Fornecedor>> ListarFornecedores();
         Task<List<Fornecedor>> FiltrarPorSetor(string setor);
     }
