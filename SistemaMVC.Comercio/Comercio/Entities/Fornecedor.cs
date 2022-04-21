80aeb91a3dad98bd543036e1589efe9f6a0d18c3
RafaelBonotto <rafareisaba@outlook.com>
2022-04-20 23:05:31 -0300

    Update Fornecedor.cs

--- SistemaMVC.Comercio/Comercio/Entities/Fornecedor.cs
+++ SistemaMVC.Comercio/Comercio/Entities/Fornecedor.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Dapper.Contrib.Extensions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Comercio.Entities
 {
+    [Table("tb_fornecedor")]
     public class Fornecedor
     {
         public int Id { get; set; }
+        public string Cnpj { get; set; }
         public string Nome_empresa { get; set; }
         public string Telefone_empresa { get; set; }
         public string Nome_vendedor{ get; set; }
@@ -14,5 +17,8 @@ namespace Comercio.Entities
         public sbyte Ativo { get; set; }
         public DateTime Data_criacao { get; set; }
         public DateTime Data_alteracao { get; set; }
+
+        [Write(false)]
+        public virtual List<Produto> Produtos { get; set; }
     }
 }
