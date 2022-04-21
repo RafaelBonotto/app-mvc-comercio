f2771ff6196913e980fe7a275589ce65d2025720
RafaelBonotto <rafareisaba@outlook.com>
2022-04-20 22:59:06 -0300

    Create CnpjInvalidoException.cs

---
+++ SistemaMVC.Comercio/Comercio/Exceptions/Fornecedor/CnpjInvalidoException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Comercio.Exceptions.Fornecedor
+{
+    public class CnpjInvalidoException : Exception
+    {
+        public CnpjInvalidoException()
+        {
+        }
+        public CnpjInvalidoException(string mensagem) : base(mensagem)
+        {
+        }
+    }
+}
