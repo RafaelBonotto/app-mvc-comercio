e66f596d4d5ea414464d31c4c3c5ddd864495d03
RafaelBonotto <rafareisaba@outlook.com>
2022-04-21 23:24:59 -0300

    Update FornecedorViewModel.cs

--- SistemaMVC.Comercio/Comercio/Models/FornecedorViewModel.cs
+++ SistemaMVC.Comercio/Comercio/Models/FornecedorViewModel.cs
@@ -8,13 +8,16 @@ namespace Comercio.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "Cnpj")]
+        public string Cnpj { get; set; }
+
         [Display(Name = "Fornecedor")]
         public string Nome_empresa { get; set; }
 
         [Display(Name = "Telefone")]
-        public List<Telefone> Telefones { get; set; }
+        public List<Telefone> Telefones { get; set; } = new();
 
         [Display(Name = "Vendedor")]
-        public List<Pessoa> Vendedores { get; set; }
+        public List<Pessoa> Vendedores { get; set; } = new();
     }
 }
