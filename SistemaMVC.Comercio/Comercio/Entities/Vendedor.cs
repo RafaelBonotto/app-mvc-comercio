755fccc5762b77be15678a2693facdffb23609a2
RafaelBonotto <rafareisaba@outlook.com>
2022-04-22 16:48:20 -0300

    Create Vendedor.cs

---
+++ SistemaMVC.Comercio/Comercio/Entities/Vendedor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Comercio.Entities
+{
+    public class Vendedor
+    {
+        public Pessoa Pessoa { get; set; } 
+        public List<Telefone> Telefones { get; set; }
+    }
+}
