63f989bbbe50cfcff8f90ba841c3e50dd1650686
RafaelBonotto <rafareisaba@outlook.com>
2022-04-22 16:47:42 -0300

    Update FornecedorQuerys.cs

--- SistemaMVC.Comercio/Comercio/Data/Querys/FornecedorQuerys.cs
+++ SistemaMVC.Comercio/Comercio/Data/Querys/FornecedorQuerys.cs
@@ -2,6 +2,8 @@
 {
     public class FornecedorQuerys
     {
-        public const string SELECT_POR_CNPJ = @"";
+        public const string SELECT_POR_CNPJ = @"SELECT * 
+                                                FROM tb_fornecedor 
+                                                WHERE tb_fornecedor.cnpj = @cnpj";
     }
 }
