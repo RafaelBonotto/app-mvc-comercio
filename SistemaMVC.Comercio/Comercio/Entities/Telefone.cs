15976494800a880896ac4bdf76734475c4489010
RafaelBonotto <rafareisaba@outlook.com>
2022-04-23 00:29:59 -0300

    Update Telefone.cs

--- SistemaMVC.Comercio/Comercio/Entities/Telefone.cs
+++ SistemaMVC.Comercio/Comercio/Entities/Telefone.cs
@@ -2,8 +2,9 @@
 
 namespace Comercio.Entities
 {
-    public class Telefone
+    public class Telefone 
     {
+        public int Id { get; set; }
         public TipoTelefone Tipo { get; set; }
         public string Ddd { get; set; }
         public string Numero { get; set; }
