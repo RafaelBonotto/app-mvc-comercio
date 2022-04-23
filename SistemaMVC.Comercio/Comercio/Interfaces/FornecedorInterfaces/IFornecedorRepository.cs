84cf74e95dd302b1fd9cd8e7f52d0d17f4f81527
RafaelBonotto <rafareisaba@outlook.com>
2022-04-22 23:34:25 -0300

    Create IFornecedorRepository.cs

---
+++ SistemaMVC.Comercio/Comercio/Interfaces/FornecedorInterfaces/IFornecedorRepository.cs
@@ -0,0 +1,12 @@
+﻿using Comercio.Entities;
+using System.Threading.Tasks;
+
+namespace Comercio.Interfaces.FornecedorInterfaces
+{
+    public interface IFornecedorRepository 
+    {
+        Task<int> InserirTelefoneFornecedor(FornecedorTelefone fornecedorTelefone);
+        Task<int> InserirEnderecoFornecedor(FornecedorEndereco fornecedorEndereco);
+        Task<int> InserirVendedorFornecedor(int fornecedor_id, Vendedor vendedor);
+    }
+}
