fc132859e633c76aafe131829649d404ffd27503
RafaelBonotto <rafareisaba@outlook.com>
2022-04-23 00:30:03 -0300

    Update IFornecedorAdapter.cs

--- SistemaMVC.Comercio/Comercio/Interfaces/FornecedorInterfaces/IFornecedorAdapter.cs
+++ SistemaMVC.Comercio/Comercio/Interfaces/FornecedorInterfaces/IFornecedorAdapter.cs
@@ -1,13 +1,15 @@
 ﻿using Comercio.Entities;
 using Comercio.Models;
+using System.Collections.Generic;
 
 namespace Comercio.Interfaces.FornecedorInterfaces
 {
     public interface IFornecedorAdapter
     {
         Fornecedor MontaFornecedorInsertRepositorio(FornecedorViewModel fornecedor);
-        FornecedorEndereco MontaFornecedorEndereco(FornecedorViewModel fornecedor);
-        FornecedorTelefone MontaFornecedorTelefone(FornecedorViewModel fornecedor);
+        //List<FornecedorEndereco> MontaFornecedorEndereco(FornecedorViewModel fornecedor);
+        //List<FornecedorTelefone> MontaFornecedorTelefone(FornecedorViewModel fornecedor);
+        //List<FornecedorVendedor> MontaVendedorFornecedor(FornecedorViewModel fornecedor);
         FornecedorViewModel CriarFornecedorViewModel(Fornecedor fornecedor);
     }
 }
