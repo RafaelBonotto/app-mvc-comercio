2212e8682eecfe17214af37f1430982dab41a4e8
RafaelBonotto <rafareisaba@outlook.com>
2022-04-22 23:34:15 -0300

    Update Pessoa.cs

--- SistemaMVC.Comercio/Comercio/Entities/Pessoa.cs
+++ SistemaMVC.Comercio/Comercio/Entities/Pessoa.cs
@@ -2,13 +2,14 @@
 
 namespace Comercio.Entities
 {
-    public class Pessoa
+    public class Pessoa 
     {
+        public int Id { get; set; }
         public string Nome { get; set; }
         public string SobreNome { get; set; }
         public string DataNasc { get; set; }
         public string Genero { get; set; }
-        public List<Telefone> Telefones { get; set; }
-        public List<Endereco> Endereco { get; set; }
+        public virtual List<Telefone> Telefones { get; set; }
+        public virtual List<Endereco> Endereco { get; set; }
     }
 }
