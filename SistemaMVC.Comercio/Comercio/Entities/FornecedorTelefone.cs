5e38d85cdd8e0fb2ae1c9896ecea5b71104c9480
RafaelBonotto <rafareisaba@outlook.com>
2022-04-24 00:05:53 -0300

    Update FornecedorTelefone.cs

--- SistemaMVC.Comercio/Comercio/Entities/FornecedorTelefone.cs
+++ SistemaMVC.Comercio/Comercio/Entities/FornecedorTelefone.cs
@@ -1,8 +1,17 @@
-﻿namespace Comercio.Entities
+﻿using Dapper.Contrib.Extensions;
+
+namespace Comercio.Entities
 {
+    [Table("tb_fornecedor_telefone")]
     public class FornecedorTelefone
-    {
+    {       
         public int Fornecedor_id { get; set; }
-        public Telefone Telefone { get; set; }
+        public int Telefone_id { get; set; }
+
+        [Write(false)]
+        public virtual Telefone Telefone { get; set; }
+
+        [Write(false)]
+        public virtual Fornecedor Fornecedor { get; set; }
     }
 }
