2a48877a9703f569e04d6f16b75755fade2569c3
RafaelBonotto <rafareisaba@outlook.com>
2022-04-24 00:23:53 -0300

    Update Pessoa.cs

--- SistemaMVC.Comercio/Comercio/Entities/Pessoa.cs
+++ SistemaMVC.Comercio/Comercio/Entities/Pessoa.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using Dapper.Contrib.Extensions;
+using System.Collections.Generic;
 
 namespace Comercio.Entities
 {
+    [Table("tb_pessoa")]
     public class Pessoa 
     {
         public int Id { get; set; }
@@ -9,7 +11,11 @@ namespace Comercio.Entities
         public string SobreNome { get; set; }
         public string DataNasc { get; set; }
         public string Genero { get; set; }
+
+        [Write(false)]
         public virtual List<Telefone> Telefones { get; set; }
+
+        [Write(false)]
         public virtual List<Endereco> Endereco { get; set; }
     }
 }
