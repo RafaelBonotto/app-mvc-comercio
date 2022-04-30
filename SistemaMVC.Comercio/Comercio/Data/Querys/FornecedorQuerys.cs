8be7cee5c850b3e4917a14e297fcbc98dd1fb8eb
RafaelBonotto <rafareisaba@outlook.com>
2022-04-29 23:19:01 -0300

    Update FornecedorQuerys.cs

--- SistemaMVC.Comercio/Comercio/Data/Querys/FornecedorQuerys.cs
+++ SistemaMVC.Comercio/Comercio/Data/Querys/FornecedorQuerys.cs
@@ -8,14 +8,23 @@
 
         public const string SELECT_ID_TELEFONE_FORNECEDOR = @"SELECT telefone_id
                                                                 FROM tb_fornecedor_telefone fornecTel
-                                                                WHERE fornecTel.fornecedor_id = @fornecedor_id";
+                                                                WHERE 
+                                                                fornecTel.fornecedor_id = @fornecedor_id
+                                                                AND 
+                                                                fornecTel.ativo = 1";
         
         public const string SELECT_ID_ENDERECO_FORNECEDOR = @"SELECT endereco_id
                                                                 FROM tb_fornecedor_endereco fornecEnd
-                                                                WHERE fornecEnd.fornecedor_id = @fornecedor_id";
+                                                                WHERE 
+                                                                fornecEnd.fornecedor_id = @fornecedor_id
+                                                                AND 
+                                                                fornecEnd.ativo = 1";
 
         public const string SELECT_ID_VENDEDOR_FORNECEDOR = @"SELECT pessoa_id
                                                                 FROM tb_fornecedor_pessoa fornecPes
-                                                                WHERE fornecPes.fornecedor_id = @fornecedor_id";
+                                                                WHERE 
+                                                                fornecPes.fornecedor_id = @fornecedor_id
+                                                                AND
+                                                                fornecPes.ativo = 1";
     }
 }
