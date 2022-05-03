131a5aa31dec184dc91ac3d5027b0514153cef79
RafaelBonotto <rafareisaba@outlook.com>
2022-05-02 22:58:44 -0300

    Create EnderecoFornecedor.cs

---
+++ SistemaMVC.Comercio/Comercio/Entities/EnderecoFornecedor.cs
@@ -0,0 +1,17 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace Comercio.Entities
+{
+    [Table("tb_endereco_fornecedor")]
+    public class EnderecoFornecedor
+    {
+        public int Fornecedor_id { get; set; }
+        public int Endereco_id { get; set; }
+
+        [Write(false)]
+        public virtual Endereco Endereco { get; set; }
+
+        [Write(false)]
+        public virtual Fornecedor Fornecedor { get; set; }
+    }
+}
