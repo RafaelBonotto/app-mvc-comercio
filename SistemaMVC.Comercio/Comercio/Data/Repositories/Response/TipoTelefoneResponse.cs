90944e229d7af1b9d53914d8463255e03459f682
RafaelBonotto <rafareisaba@outlook.com>
2022-05-07 00:09:05 -0300

    Create TipoTelefoneResponse.cs

---
+++ SistemaMVC.Comercio/Comercio/Data/Repositories/Response/TipoTelefoneResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Comercio.Data.Repositories.Response
+{
+    public class TipoTelefoneResponse
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
