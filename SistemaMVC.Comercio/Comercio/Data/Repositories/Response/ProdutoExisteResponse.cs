3ca8028e6d9f619dc7e1251c852823e22c2b847a
RafaelBonotto <rafareisaba@outlook.com>
2022-05-07 00:08:56 -0300

    Create ProdutoExisteResponse.cs

---
+++ SistemaMVC.Comercio/Comercio/Data/Repositories/Response/ProdutoExisteResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Comercio.Data.Repositories.Response
+{
+    public class ProdutoExisteResponse
+    {
+        public bool Existe { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
