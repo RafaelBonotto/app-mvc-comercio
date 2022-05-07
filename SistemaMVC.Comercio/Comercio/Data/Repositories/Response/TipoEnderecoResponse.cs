793b05a09b05d86ccae26f11aaf963cb4af0c27d
RafaelBonotto <rafareisaba@outlook.com>
2022-05-07 00:09:02 -0300

    Create TipoEnderecoResponse.cs

---
+++ SistemaMVC.Comercio/Comercio/Data/Repositories/Response/TipoEnderecoResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Comercio.Data.Repositories.Response
+{
+    public class TipoEnderecoResponse
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
