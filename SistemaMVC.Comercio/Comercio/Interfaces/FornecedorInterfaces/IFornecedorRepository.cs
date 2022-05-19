664ee285630cfebf08475478241f4d14012365f7
RafaelBonotto <rafareisaba@outlook.com>
2022-05-18 23:21:55 -0300

    Update IFornecedorRepository.cs

--- SistemaMVC.Comercio/Comercio/Interfaces/FornecedorInterfaces/IFornecedorRepository.cs
+++ SistemaMVC.Comercio/Comercio/Interfaces/FornecedorInterfaces/IFornecedorRepository.cs
@@ -8,6 +8,7 @@ namespace Comercio.Interfaces.FornecedorInterfaces
     public interface IFornecedorRepository
     {
         Task InserirTelefone(int fornecedor_id, Telefone telefone); 
+        Task ExcluirTelefone(int fornecedor_id, int telefone_id); 
         Task InserirEndereco(int fornecedor_id, Endereco endereco);
         Task InserirVendedor(int fornecedor_id, Vendedor vendedor);
         Task<int> ObterIdTipoTelefone(string tipoTelefone);
