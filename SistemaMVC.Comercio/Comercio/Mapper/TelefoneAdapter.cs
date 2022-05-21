ce60512cdd98f9a9bd439d1615dc12053000c0f8
RafaelBonotto <rafareisaba@outlook.com>
2022-05-21 17:09:25 -0300

    Create TelefoneAdapter.cs

---
+++ SistemaMVC.Comercio/Comercio/Mapper/TelefoneAdapter.cs
@@ -0,0 +1,21 @@
+﻿using Comercio.Entities;
+using Comercio.Interfaces.TelefoneInterfaces;
+using System;
+
+namespace Comercio.Mapper
+{
+    public class TelefoneAdapter : ITelefoneAdapter
+    {
+        public TelefoneFornecedor MontaTelefoneFornecedor(int fornecedorId, int telefoneId)
+        {
+            return new TelefoneFornecedor()
+            {
+                Fornecedor_id = fornecedorId,
+                Telefone_id = telefoneId,
+                Ativo = 1,
+                Data_criacao = DateTime.Now,
+                Data_alteracao = DateTime.Now
+            };
+        }
+    }
+}
