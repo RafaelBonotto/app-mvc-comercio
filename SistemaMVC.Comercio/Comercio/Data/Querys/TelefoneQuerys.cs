4b0d3a5d37d0bc378eef604b30c33263ddd8269b
RafaelBonotto <rafareisaba@outlook.com>
2022-05-21 16:42:38 -0300

    Update TelefoneQuerys.cs

--- SistemaMVC.Comercio/Comercio/Data/Querys/TelefoneQuerys.cs
+++ SistemaMVC.Comercio/Comercio/Data/Querys/TelefoneQuerys.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Comercio.Data.Querys
+﻿namespace Comercio.Data.Querys
 {
     public class TelefoneQuerys
     {
+        public const string SELECT_ID_TIPO_TELEFONE = @"SELECT id 
+                                                        FROM tb_tipo_telefone tpTel
+                                                        WHERE tpTel.descricao = @tipoTelefone";
 
+        public const string SELECT_TIPO_TELEFONE = @"SELECT id, descricao 
+                                                    FROM tb_tipo_telefone";
+
+        public const string DESATIVAR_TELEFONE_FORNECEDOR = @"UPDATE tb_telefone_fornecedor fornecTel
+                                                                SET fornecTel.Ativo = 0
+                                                                WHERE fornecTel.fornecedor_id = @fornecedor_id
+                                                                AND fornecTel.telefone_id = @telefone_id";
     }
 }
