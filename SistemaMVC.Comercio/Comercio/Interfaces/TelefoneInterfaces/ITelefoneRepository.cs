c4d69d41ee756a8423689840b0baaab3493a4900
RafaelBonotto <rafareisaba@outlook.com>
2022-05-21 16:40:20 -0300

    Update ITelefoneRepository.cs

--- SistemaMVC.Comercio/Comercio/Interfaces/TelefoneInterfaces/ITelefoneRepository.cs
+++ SistemaMVC.Comercio/Comercio/Interfaces/TelefoneInterfaces/ITelefoneRepository.cs
@@ -1,7 +1,14 @@
-﻿namespace Comercio.Interfaces.TelefoneInterfaces
+﻿using Comercio.Entities;
+using System.Collections.Generic;
+
+namespace Comercio.Interfaces.TelefoneInterfaces
 {
     public interface ITelefoneRepository
     {
-
+        bool EditarTelefone(int id);
+        List<string> ListarDescricaoTipoTelefone();
+        bool InserirTelefoneFornecedor(int fornecedor_id, Telefone telefone);
+        bool ExcluirTelefoneFornecedor(int fornecedor_id, int telefone_id);
+        List<Telefone> ListarTelefoneFornecedor(int fornecedor_id);
     }
 }
