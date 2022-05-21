8581e2c5324813ee18b890a027cd8a8358b34489
RafaelBonotto <rafareisaba@outlook.com>
2022-05-21 01:08:26 -0300

    Create TelefoneRepository.cs

---
+++ SistemaMVC.Comercio/Comercio/Data/Repositories/Telefones/TelefoneRepository.cs
@@ -0,0 +1,15 @@
+﻿using Comercio.Data.ConnectionManager;
+using Comercio.Interfaces.TelefoneInterfaces;
+
+namespace Comercio.Data.Repositories.Telefones
+{
+    public class TelefoneRepository : ITelefoneRepository
+    {
+        private readonly IMySqlConnectionManager _connection;
+
+        public TelefoneRepository(IMySqlConnectionManager connection)
+        {
+            _connection = connection;
+        }
+    }
+}
