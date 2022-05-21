8c26ac9c57301d8608dbba0651a1fc5411e347e0
RafaelBonotto <rafareisaba@outlook.com>
2022-05-21 16:40:14 -0300

    Update TelefoneRepository.cs

--- SistemaMVC.Comercio/Comercio/Data/Repositories/Telefones/TelefoneRepository.cs
+++ SistemaMVC.Comercio/Comercio/Data/Repositories/Telefones/TelefoneRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Comercio.Data.ConnectionManager;
+using Comercio.Entities;
 using Comercio.Interfaces.TelefoneInterfaces;
+using System.Collections.Generic;
 
 namespace Comercio.Data.Repositories.Telefones
 {
@@ -10,6 +12,31 @@ namespace Comercio.Data.Repositories.Telefones
         public TelefoneRepository(IMySqlConnectionManager connection)
         {
             _connection = connection;
+        }
+
+        public bool EditarTelefone(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool ExcluirTelefoneFornecedor(int fornecedor_id, int telefone_id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool InserirTelefoneFornecedor(int fornecedor_id, Telefone telefone)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<string> ListarDescricaoTipoTelefone()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Telefone> ListarTelefoneFornecedor(int fornecedor_id)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
