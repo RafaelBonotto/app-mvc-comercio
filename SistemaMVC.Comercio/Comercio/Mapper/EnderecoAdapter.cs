1609ec7a9192b15737b6e6fc13f2c0bf7ef57948
RafaelBonotto <rafareisaba@outlook.com>
2022-05-23 17:12:31 -0300

    Update EnderecoAdapter.cs

--- SistemaMVC.Comercio/Comercio/Mapper/EnderecoAdapter.cs
+++ SistemaMVC.Comercio/Comercio/Mapper/EnderecoAdapter.cs
@@ -1,8 +1,21 @@
-﻿using Comercio.Interfaces.EnderecoInterfaces;
+﻿using Comercio.Entities;
+using Comercio.Interfaces.EnderecoInterfaces;
+using System;
 
 namespace Comercio.Mapper
 {
     public class EnderecoAdapter : IEnderecoAdapter
     {
+        public EnderecoFornecedor MontaInsertEnderecoFornecedor(int fornecedorId, int enderecoId)
+        {
+            return new EnderecoFornecedor()
+            {
+                Fornecedor_id = fornecedorId,
+                Endereco_id = enderecoId,
+                Ativo = 1,
+                Data_criacao = DateTime.Now,
+                Data_alteracao = DateTime.Now
+            };
+        }
     }
 }
