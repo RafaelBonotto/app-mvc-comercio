910f99860e1b7138df387a00ec27c714131c506d
RafaelBonotto <rafareisaba@outlook.com>
2022-05-23 00:00:23 -0300

    Create EnderecoRepository.cs

---
+++ SistemaMVC.Comercio/Comercio/Data/Repositories/Enderecos/EnderecoRepository.cs
@@ -0,0 +1,15 @@
+﻿using Comercio.Data.ConnectionManager;
+using Comercio.Interfaces.EnderecoInterfaces;
+
+namespace Comercio.Data.Repositories.Enderecos
+{
+    public class EnderecoRepository : IEnderecoRepository
+    {
+        private readonly IMySqlConnectionManager _connection;
+
+        public EnderecoRepository(IMySqlConnectionManager connection)
+        {
+            _connection = connection;
+        }
+    }
+}
