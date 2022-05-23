394d3d114e6f630d379c47bdd44dd555effc41ae
RafaelBonotto <rafareisaba@outlook.com>
2022-05-23 17:12:12 -0300

    Update IEnderecoRepository.cs

--- SistemaMVC.Comercio/Comercio/Interfaces/EnderecoInterfaces/IEnderecoRepository.cs
+++ SistemaMVC.Comercio/Comercio/Interfaces/EnderecoInterfaces/IEnderecoRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace Comercio.Interfaces.EnderecoInterfaces
+﻿using Comercio.Data.Repositories.Response;
+using Comercio.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Comercio.Interfaces.EnderecoInterfaces
 {
     public interface IEnderecoRepository
     {
+        Task InserirEnderecoFornecedor(int fornecedor_id, Endereco endereco);
+        Task<int> ObterIdTipoEndereco(string tipoEndereco);
+        Task<List<TipoEnderecoResponse>> ObterDescricaoTipoEndereco();
+        Task<List<Endereco>> ObterEnderecoDoFornecedor(int fornecedor_id);
     }
 }
