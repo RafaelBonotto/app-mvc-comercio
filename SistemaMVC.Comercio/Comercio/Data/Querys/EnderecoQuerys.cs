8f5e81f4f81e3cee2a93356405c4fbd4037ad566
RafaelBonotto <rafareisaba@outlook.com>
2022-05-23 17:11:26 -0300

    Update EnderecoQuerys.cs

--- SistemaMVC.Comercio/Comercio/Data/Querys/EnderecoQuerys.cs
+++ SistemaMVC.Comercio/Comercio/Data/Querys/EnderecoQuerys.cs
@@ -2,6 +2,18 @@
 {
     public class EnderecoQuerys
     {
+        public const string SELECT_ID_ENDERECO_FORNECEDOR = @"SELECT endereco_id
+                                                                FROM tb_endereco_fornecedor fornecEnd
+                                                                WHERE 
+                                                                fornecEnd.fornecedor_id = @fornecedor_id
+                                                                AND 
+                                                                fornecEnd.ativo = 1";
 
+        public const string SELECT_ID_TIPO_ENDERECO = @"SELECT id 
+                                                        FROM tb_tipo_endereco tpEnd
+                                                        WHERE tpEnd.descricao = @tipoEndereco";
+
+        public const string SELECT_TIPO_ENDERECO = @"SELECT id, descricao 
+                                                    FROM tb_tipo_endereco";
     }
 }
