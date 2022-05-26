6a4bdbd1e27e7f4123e93bd0cd27766cd473dbf9
RafaelBonotto <rafareisaba@outlook.com>
2022-05-25 23:53:43 -0300

    Create TelefoneFornecedorViewModel.cs

---
+++ SistemaMVC.Comercio/Comercio/Models/TelefoneFornecedorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Comercio.Models
+{
+    public class TelefoneFornecedorViewModel
+    {
+        public int Fornecedor_id { get; set; }
+        public int Telefone_id { get; set; }
+        public string Ddd { get; set; }
+        public string Numero { get; set; }
+        public string Tipo_telefone { get; set; }
+        public IEnumerable<SelectListItem> TiposTelefoneBanco { get; set; }
+    }
+}
