5591dcde17d1587e27165b1868bb65b9719060bc
RafaelBonotto <rafareisaba@outlook.com>
2022-05-27 22:53:29 -0300

    Update EnderecoFornecedorViewModel.cs

--- SistemaMVC.Comercio/Comercio/Models/EnderecoFornecedorViewModel.cs
+++ SistemaMVC.Comercio/Comercio/Models/EnderecoFornecedorViewModel.cs
@@ -1,6 +1,22 @@
-﻿namespace Comercio.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Comercio.Models
 {
     public class EnderecoFornecedorViewModel
     {
+        public int Fornecedor_id { get; set; }
+        public int Endereco_id { get; set; }
+        public int Tipo_endereco_id { get; set; }
+        public string Tipo_endereco { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Cep { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string UF { get; set; }
+        public IEnumerable<SelectListItem> TiposEnderecoBanco { get; set; }
     }
 }
