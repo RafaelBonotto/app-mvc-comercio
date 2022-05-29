67ce384600e24d6a67e213d189041d8e294693d9
RafaelBonotto <rafareisaba@outlook.com>
2022-05-29 00:37:45 -0300

    Update IVendedorAdapter.cs

--- SistemaMVC.Comercio/Comercio/Interfaces/VendedorInterfaces/IVendedorAdapter.cs
+++ SistemaMVC.Comercio/Comercio/Interfaces/VendedorInterfaces/IVendedorAdapter.cs
@@ -1,6 +1,9 @@
-﻿namespace Comercio.Interfaces.VendedorInterfaces
+﻿using Comercio.Entities;
+
+namespace Comercio.Interfaces.VendedorInterfaces
 {
     public interface IVendedorAdapter
     {
+        VendedorFornecedor MontaVendedorFornecedor(int fornecedor_id, int pessoa_id);
     }
 }
