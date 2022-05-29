f48584f3c5d9dc5bf667c6c00458654b736d74d4
RafaelBonotto <rafareisaba@outlook.com>
2022-05-29 00:07:17 -0300

    Create VendedorRepository.cs

---
+++ SistemaMVC.Comercio/Comercio/Data/Repositories/Vendedores/VendedorRepository.cs
@@ -0,0 +1,19 @@
+﻿using Comercio.Data.ConnectionManager;
+using Comercio.Interfaces.VendedorInterfaces;
+
+namespace Comercio.Data.Repositories.Vendedores
+{
+    public class VendedorRepository : IVendedorRepository
+    {
+        private readonly IMySqlConnectionManager _connection;
+        private readonly IVendedorAdapter _mapperVendedor;
+
+        public VendedorRepository(
+            IMySqlConnectionManager connection, 
+            IVendedorAdapter mapperVendedor)
+        {
+            _connection = connection;
+            _mapperVendedor = mapperVendedor;
+        }
+    }
+}
