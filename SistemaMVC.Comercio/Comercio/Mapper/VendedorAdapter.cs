615b06e5afd46f9b1403ed326c7bd2bc6f985231
RafaelBonotto <rafareisaba@outlook.com>
2022-06-01 22:58:48 -0300

    Update VendedorAdapter.cs

--- SistemaMVC.Comercio/Comercio/Mapper/VendedorAdapter.cs
+++ SistemaMVC.Comercio/Comercio/Mapper/VendedorAdapter.cs
@@ -1,8 +1,21 @@
-﻿using Comercio.Interfaces.VendedorInterfaces;
+﻿using Comercio.Entities;
+using Comercio.Interfaces.VendedorInterfaces;
+using System;
 
 namespace Comercio.Mapper
 {
     public class VendedorAdapter : IVendedorAdapter
     {
+        public VendedorFornecedor MontaVendedorFornecedor(int fornecedor_id, int pessoa_id)
+        {
+            return new VendedorFornecedor
+            {
+                Fornecedor_id = fornecedor_id,
+                Pessoa_id = pessoa_id,
+                Ativo = 1,
+                Data_criacao = DateTime.Now,
+                Data_alteracao = DateTime.Now
+            };
+        }
     }
 }
