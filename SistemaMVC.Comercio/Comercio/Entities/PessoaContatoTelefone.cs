d28ab6e65a56cfbc2ff2e5d458b6983efe019ac3
RafaelBonotto <rafareisaba@outlook.com>
2022-06-01 23:31:30 -0300

    Create PessoaContatoTelefone.cs

---
+++ SistemaMVC.Comercio/Comercio/Entities/PessoaContatoTelefone.cs
@@ -0,0 +1,15 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+
+namespace Comercio.Entities
+{
+    [Table("tb_pessoa_contato_telefone")]
+    public class PessoaContatoTelefone
+    {
+        public int Pessoa_contato_id { get; set; }
+        public int Telefone_id { get; set; }
+        public sbyte Ativo { get; set; }
+        public DateTime Data_criacao { get; set; }
+        public DateTime Data_alteracao { get; set; } 
+    }
+}
