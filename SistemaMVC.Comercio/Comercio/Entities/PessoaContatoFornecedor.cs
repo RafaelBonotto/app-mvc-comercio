7f487d80693d18a05e835370beed0c221476b017
RafaelBonotto <rafareisaba@outlook.com>
2022-06-01 22:58:37 -0300

    Create PessoaContatoFornecedor.cs

---
+++ SistemaMVC.Comercio/Comercio/Entities/PessoaContatoFornecedor.cs
@@ -0,0 +1,15 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+
+namespace Comercio.Entities
+{
+    [Table("tb_pessoa_contato_fornecedor")]
+    public class PessoaContatoFornecedor
+    {
+        public int Fornecedor_id { get; set; }
+        public int PessoaContato_id { get; set; }
+        public sbyte Ativo { get; set; }
+        public DateTime Data_criacao { get; set; }
+        public DateTime Data_alteracao { get; set; }
+    }
+}
