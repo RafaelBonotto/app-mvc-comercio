ba37050c8df4f834f79119587b55c9b169c5a8e9
RafaelBonotto <rafareisaba@outlook.com>
2022-06-03 22:51:20 -0300

    Update IFornecedorAdapter.cs

--- SistemaMVC.Comercio/Comercio/Interfaces/FornecedorInterfaces/IFornecedorAdapter.cs
+++ SistemaMVC.Comercio/Comercio/Interfaces/FornecedorInterfaces/IFornecedorAdapter.cs
@@ -10,7 +10,7 @@ namespace Comercio.Interfaces.FornecedorInterfaces
     {
         Fornecedor MontaFornecedorInsertRepositorio(FornecedorViewModel fornecedor);
         Telefone MontaUpdateTelefone(int telefone_id, string ddd, string numero, int tipoTelefone_id);
-        Telefone MontaInsertTelefone(string ddd, string numero);
+        Telefone MontaInsertTelefoneVendedor(string ddd, string numero);
         Endereco MontarInsertEndereco(
             string logradouro, string numero, string complemento, string cep, 
             string bairro, string cidade, string estado, string uf);
