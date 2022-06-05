c8bc35083fad9daba9b2d64e88b3b44a5c906096
RafaelBonotto <rafareisaba@outlook.com>
2022-06-05 00:19:30 -0300

    Update VendedorFornecedorViewModel.cs

--- SistemaMVC.Comercio/Comercio/Models/VendedorFornecedorViewModel.cs
+++ SistemaMVC.Comercio/Comercio/Models/VendedorFornecedorViewModel.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Comercio.Models
+﻿namespace Comercio.Models
 {
     public class VendedorFornecedorViewModel
     {
+        public int Fornecedor_id { get; set; }
+        public int Vendedor_id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Ddd { get; set; }
+        public string Numero { get; set; }
+        public string DddAdicional { get; set; }
+        public string NumeroAdicional { get; set; }
     }
 }
