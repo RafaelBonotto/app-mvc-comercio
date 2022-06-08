eedca856da2258ea9d7dbaf930b0c0cac06ebd22
RafaelBonotto <rafareisaba@outlook.com>
2022-06-08 18:08:42 -0300

    Update IFornecedorService.cs

--- SistemaMVC.Comercio/Comercio/Interfaces/FornecedorInterfaces/IFornecedorService.cs
+++ SistemaMVC.Comercio/Comercio/Interfaces/FornecedorInterfaces/IFornecedorService.cs
@@ -10,7 +10,7 @@ namespace Comercio.Interfaces.FornecedorInterfaces
     public interface IFornecedorService
     {
         Task<Fornecedor> InserirFornecedor(FornecedorViewModel fornecedor);
-        Task<bool> InserirVendedor(AdicionarVendedorRequest request);
+        Task<bool> InserirVendedor(VendedorRequest request);
         Task<bool> InserirTelefone(int fornecedor_id, string ddd, string numero, string tipoTelefone);
         Task<bool> EditarTelefone(int telefone_id, string ddd, string numero, string tipoTelefone);
         Task<bool> ExcluirTelefone(int fornecedor_id, int telefone_id);
@@ -18,6 +18,7 @@ namespace Comercio.Interfaces.FornecedorInterfaces
             string complemento, string cep, string bairro, string cidade, string estado, string uf, string tipoEndereco);
         Task<bool> EditarEndereco(int endereco_id, string logradouro, string numero,
             string complemento, string cep, string bairro, string cidade, string estado, string uf, string tipoEndereco);
+        Task<bool> EditarVendedor(VendedorRequest request);
         Task<bool> ExcluirEndereco(int fornecedor_id, int endereco_id);
         Task<List<Fornecedor>> ListarFornecedores();
         Task<List<Fornecedor>> FiltrarPorSetor(string setor);
