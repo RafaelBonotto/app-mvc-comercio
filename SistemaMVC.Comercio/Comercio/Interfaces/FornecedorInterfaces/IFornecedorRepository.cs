88b1da252e2c63f7ffce26677b24242075719317
RafaelBonotto <rafareisaba@outlook.com>
2022-06-09 23:16:28 -0300

    Update IFornecedorRepository.cs

--- SistemaMVC.Comercio/Comercio/Interfaces/FornecedorInterfaces/IFornecedorRepository.cs
+++ SistemaMVC.Comercio/Comercio/Interfaces/FornecedorInterfaces/IFornecedorRepository.cs
@@ -12,6 +12,6 @@ namespace Comercio.Interfaces.FornecedorInterfaces
         Task<bool> InserirVendedor(int fornecedor_id, PessoaContato vendedor, List<Telefone> telefones);
         Task<bool> AtualizarVendedor(VendedorRequest vendedor);
         Task<PessoaContato> GetVendedor(int vendedor_id);
-        Task<List<Telefone>> GetTelefoneVendedor(int vendedor_id, MySqlConnection connection = null);
+        Task<List<Telefone>> GetTelefoneVendedor(int vendedor_id, MySqlConnection connection = null, MySqlTransaction transaction = null);
     }
 }
