7d65cdbda7440e687db896ad7d7f2dbaaf1cfdc4
RafaelBonotto <rafareisaba@outlook.com>
2022-06-11 00:31:17 -0300

    Update PessoaContatoFornecedor.cs

--- SistemaMVC.Comercio/Comercio/Entities/PessoaContatoFornecedor.cs
+++ SistemaMVC.Comercio/Comercio/Entities/PessoaContatoFornecedor.cs
@@ -6,6 +6,7 @@ namespace Comercio.Entities
     [Table("tb_pessoa_contato_fornecedor")]
     public class PessoaContatoFornecedor
     {
+        public int Id { get; set; }
         public int Fornecedor_id { get; set; }
         public int Pessoa_Contato_id { get; set; }
         public sbyte Ativo { get; set; }
