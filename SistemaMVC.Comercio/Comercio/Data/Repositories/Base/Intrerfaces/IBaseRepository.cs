96b6aac01a243ed1e200bd910c605bab6d8fcd8c
RafaelBonotto <rafareisaba@outlook.com>
2022-06-17 18:09:06 -0300

    Create IBaseRepository.cs

---
+++ SistemaMVC.Comercio/Comercio/Data/Repositories/Base/Intrerfaces/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Comercio.Data.Repositories.Base.Intrerfaces
+{
+    public interface IBaseRepository<T>
+    {
+        Task<List<T>> GetAllAsync();
+        Task<List<T>> GetByKeyAsync(string key);
+        Task<T> GetByIdAsync(int id);
+        Task<bool> AddAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
