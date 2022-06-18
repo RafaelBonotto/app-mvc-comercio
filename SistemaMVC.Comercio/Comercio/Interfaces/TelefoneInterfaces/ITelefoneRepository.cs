a705a4d3987a46f9346a975e0f5260d7a860a404
RafaelBonotto <rafareisaba@outlook.com>
2022-06-18 01:25:53 -0300

    Update ITelefoneRepository.cs

--- SistemaMVC.Comercio/Comercio/Interfaces/TelefoneInterfaces/ITelefoneRepository.cs
+++ SistemaMVC.Comercio/Comercio/Interfaces/TelefoneInterfaces/ITelefoneRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Comercio.Data.Repositories.Response;
 using Comercio.Entities;
+using MySqlConnector;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -9,10 +10,10 @@ namespace Comercio.Interfaces.TelefoneInterfaces
     {
         Task<bool> AtualizarTelefone(Telefone telefone);
         Task<int> ObterIdTipoTelefone(string tipoTelefone);
-        Task<List<TipoTelefoneResponse>> ListarDescricaoTipoTelefone();
+        Task<List<TipoTelefoneResponse>> ListarDescricaoTipoTelefone(MySqlConnection connection = null);
         Task<bool> InserirTelefoneFornecedor(int fornecedor_id, Telefone telefone);
         Task<bool> ExcluirTelefoneFornecedor(int fornecedor_id, int telefone_id);
-        Task<List<Telefone>> ListarTelefoneFornecedor(int fornecedor_id);
+        Task<List<Telefone>> ListarTelefoneFornecedor(int fornecedor_id, MySqlConnection conn = null);
         Task<Telefone> GetById(int telefone_id);
     }
 }
