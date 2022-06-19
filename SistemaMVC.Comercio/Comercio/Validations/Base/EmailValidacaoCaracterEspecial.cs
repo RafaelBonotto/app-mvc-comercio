3612b4eee70371a1bd8456a1632a90cfffa1008f
RafaelBonotto <rafareisaba@outlook.com>
2022-06-19 00:04:43 -0300

    Create EmailValidacaoCaracterEspecial.cs

---
+++ SistemaMVC.Comercio/Comercio/Validations/Base/EmailValidacaoCaracterEspecial.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Comercio.Validations.Base
+{
+    public class EmailValidacaoCaracterEspecial : ValidationAttribute
+    {
+        public string Email { get; set; }
+        public string GetErrorMessage() => $"Email Inválido: {Email}";
+
+        protected override ValidationResult IsValid(object Value, ValidationContext validationContext)
+        {
+            var aux = (dynamic)validationContext.ObjectInstance;
+
+            if (string.IsNullOrEmpty(aux.Email))
+                return ValidationResult.Success;
+
+            if (CaracterEspecial(aux.Email))
+                return new ValidationResult(GetErrorMessage());
+
+            return ValidationResult.Success;
+        }
+
+        public bool CaracterEspecial(string email)
+        {
+            var caracterInvalidos = "!@#$%¨&*()-+=/*,?|~áéíóúãõàèìòùâêîôû[{]}äëïöü\"'< >^";
+            foreach (var caracter in email)
+                foreach (var charr in caracterInvalidos)
+                    if (charr.Equals(caracter))
+                        return true;
+            return false;
+        }
+    }
+
+}
