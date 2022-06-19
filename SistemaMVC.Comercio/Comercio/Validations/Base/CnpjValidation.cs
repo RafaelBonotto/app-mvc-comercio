73d699f1501e0123b736ad90c9368e81fa1896d1
RafaelBonotto <rafareisaba@outlook.com>
2022-06-19 00:04:38 -0300

    Create CnpjValidation.cs

---
+++ SistemaMVC.Comercio/Comercio/Validations/Base/CnpjValidation.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Comercio.Validations.Base
+{
+    public class CnpjValidation : ValidationAttribute
+    {
+        public string Cnpj { get; set; }
+        public string GetErrorMessage() => $"CNPJ Inválido: {Cnpj}";
+
+        protected override ValidationResult IsValid(object Value, ValidationContext validationContext)
+        {
+            var aux = (dynamic)validationContext.ObjectInstance;
+
+            if (!this.ValidarCnpj(aux.Cnpj))
+                return new ValidationResult(GetErrorMessage());
+
+            return ValidationResult.Success;
+        }
+
+        public bool ValidarCnpj(string cnpj)
+        {
+            int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int soma;
+            int resto;
+            string digito;
+            string tempCnpj;
+            cnpj = cnpj.Trim();
+            cnpj = cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
+            if (cnpj.Length != 14)
+                return false;
+            tempCnpj = cnpj.Substring(0, 12);
+            soma = 0;
+            for (int i = 0; i < 12; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCnpj = tempCnpj + digito;
+            soma = 0;
+            for (int i = 0; i < 13; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return cnpj.EndsWith(digito);
+        }
+    }
+}
