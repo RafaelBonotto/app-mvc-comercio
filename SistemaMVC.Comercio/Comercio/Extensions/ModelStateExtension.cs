606092e245682c57b66488cd4960109b94820067
RafaelBonotto <rafareisaba@outlook.com>
2022-06-22 15:05:30 -0300

    Create ModelStateExtension.cs

---
+++ SistemaMVC.Comercio/Comercio/Extensions/ModelStateExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Comercio.Extensions
+{
+    public static class ModelStateExtension
+    {
+        public static List<string> GetErros(this ModelStateDictionary modelState)
+        {
+            return modelState.Where(a => a.Value.ValidationState == ModelValidationState.Invalid)
+                            .Select(a => a.Key + " - " + a.Value.Errors[0].ErrorMessage)
+                            .ToList();
+        }
+    }
+}
