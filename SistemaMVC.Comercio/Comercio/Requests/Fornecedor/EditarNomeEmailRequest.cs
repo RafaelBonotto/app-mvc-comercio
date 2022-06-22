d14a718c5d61e8bfc47b4cfc0c641259458b57b1
RafaelBonotto <rafareisaba@outlook.com>
2022-06-22 15:06:01 -0300

    Create EditarNomeEmailRequest.cs

---
+++ SistemaMVC.Comercio/Comercio/Requests/Fornecedor/EditarNomeEmailRequest.cs
@@ -0,0 +1,18 @@
+﻿using Comercio.Validations.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace Comercio.Requests.Fornecedor
+{
+    public class EditarNomeEmailRequest
+    {
+        public int Fornecedor_id { get; set; }
+
+        [MaxLength(100)]
+        [Required(ErrorMessage = "Campo Nome obrigatório")]
+        public string Nome { get; set; }
+
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        [EmailValidacaoCaracterEspecial]
+        public string Email { get; set; }
+    }
+}
