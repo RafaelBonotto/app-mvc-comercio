65e58db98a40886ffd4d40965606ce2aa0c454b6
RafaelBonotto <rafareisaba@outlook.com>
2022-06-25 01:06:21 -0300

    Create TelefoneNumeroValidacao.cs

---
+++ SistemaMVC.Comercio/Comercio/Validations/Telefone/TelefoneNumeroValidacao.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Comercio.Validations.Telefone
+{
+    public class TelefoneNumeroValidacao : ValidationAttribute
+    {
+        public string Numero { get; set; }
+        private string GetErrorMessage() => $"Número Inválido";
+
+        protected override ValidationResult IsValid(object Value, ValidationContext validationContext)
+        {
+            var aux = (dynamic)validationContext.ObjectInstance;
+
+            if (!ValidarNumero(aux.Numero))
+                return new ValidationResult(GetErrorMessage());
+
+            return ValidationResult.Success;
+        }
+
+        public bool ValidarNumero(string numero)
+        {
+            return false;
+        }
+    }
+}
