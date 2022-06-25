8e8e5c463951703d3c0fca8d8b14a032af15aa91
RafaelBonotto <rafareisaba@outlook.com>
2022-06-25 01:03:28 -0300

    Create TelefoneDddValidacao.cs

---
+++ SistemaMVC.Comercio/Comercio/Validations/Telefone/TelefoneDddValidacao.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Comercio.Validations.Telefone
+{
+    public class TelefoneDddValidacao : ValidationAttribute
+    {
+        public string Ddd { get; set; }
+        private string GetErrorMessage() => $"DDD Inválido";
+
+        protected override ValidationResult IsValid(object Value, ValidationContext validationContext)
+        {
+            var aux = (dynamic)validationContext.ObjectInstance;
+
+            if (!ValidarDdd(aux.Ddd))
+                return new ValidationResult(GetErrorMessage());
+
+            return ValidationResult.Success;
+        }
+
+        public bool ValidarDdd(string ddd)
+        {
+            return false;
+        }
+    }
+}
