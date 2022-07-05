7bd98e66006351140127e09849f047ce0d39467b
RafaelBonotto <rafareisaba@outlook.com>
2022-07-04 23:59:35 -0300

    Update IFornecedorService.cs

--- SistemaMVC.Comercio/Comercio/Interfaces/FornecedorInterfaces/IFornecedorService.cs
+++ SistemaMVC.Comercio/Comercio/Interfaces/FornecedorInterfaces/IFornecedorService.cs
@@ -18,7 +18,7 @@ namespace Comercio.Interfaces.FornecedorInterfaces
         Task<Fornecedor> EditarEndereco(EnderecoRequest endereco);
         Task<bool> EditarVendedor(VendedorRequest request);
         Task<bool> ExcluirVendedor(int fornecedor_id, int vendedor_id);
-        Task<bool> ExcluirEndereco(int fornecedor_id, int endereco_id);
+        Task<Fornecedor> ExcluirEndereco(int fornecedor_id, int endereco_id);
         Task<Fornecedor> EditarNomeEmail(int fornecedor_id, string nome, string email);
         Task<List<Fornecedor>> ListarFornecedores();
         Task<List<Fornecedor>> FiltrarPorSetor(string setor);
