1a05a5c71cb7af64d7a350dc1ce9e70ce5d6a487
RafaelBonotto <rafareisaba@outlook.com>
2022-07-04 23:59:15 -0300

    Update IEnderecoRepository.cs

--- SistemaMVC.Comercio/Comercio/Interfaces/EnderecoInterfaces/IEnderecoRepository.cs
+++ SistemaMVC.Comercio/Comercio/Interfaces/EnderecoInterfaces/IEnderecoRepository.cs
@@ -12,7 +12,7 @@ namespace Comercio.Interfaces.EnderecoInterfaces
         Task<bool> AtualizarEndereco(EnderecoRequest endereco, MySqlConnection connection = null);
         Task<Endereco> GetById(int id);
         Task<bool> InserirEnderecoFornecedor(int fornecedor_id, Endereco endereco, MySqlConnection connection = null);
-        Task<bool> ExcluirEnderecoFornecedor(int fornecedor_id, int endereco_id);
+        Task<bool> ExcluirEnderecoFornecedor(int fornecedor_id, int endereco_id, MySqlConnection connection = null);
         Task<int> ObterIdTipoEndereco(string tipoEndereco, MySqlConnection connection = null);
         Task<List<TipoEnderecoResponse>> ObterDescricaoTipoEndereco(MySqlConnection connection = null);
         Task<List<Endereco>> ObterEnderecoFornecedor(int fornecedor_id, MySqlConnection conn = null);
