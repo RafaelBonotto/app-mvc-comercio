8f03175b62c8f2498ae0a98150b96aa066e56c9b
RafaelBonotto <rafareisaba@outlook.com>
2022-07-24 01:23:35 -0300

    Create ListaFornecedorResponse.cs

---
+++ SistemaMVC.Comercio/Comercio/Responses/Produto/ListaFornecedorResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Comercio.Responses.Produto
+{
+    public class ListaFornecedorResponse
+    {
+        public int Id { get; set; } 
+        public string Cnpj { get; set; }
+        public string Nome_empresa{ get; set; } 
+    }
+}
