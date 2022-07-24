a60183e69547dcf70dbced572f437966f5987001
RafaelBonotto <rafareisaba@outlook.com>
2022-07-24 01:23:08 -0300

    Create FornecedorProduto.cs

---
+++ SistemaMVC.Comercio/Comercio/Entities/FornecedorProduto.cs
@@ -0,0 +1,12 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace Comercio.Entities
+{
+    [Table("tb_fornecedor_produto")]
+    public class FornecedorProduto
+    {
+        public int Id { get; set; }
+        public int Fornecedor_id { get; set; }
+        public int Produto_id { get; set; }
+    }
+}
