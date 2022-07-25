795b2eaa1a19592e35b97f8959c155dc8c49b045
RafaelBonotto <rafareisaba@outlook.com>
2022-07-25 18:06:21 -0300

    Create ListarFornecedorViewModel.cs

---
+++ SistemaMVC.Comercio/Comercio/Models/ListarFornecedorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comercio.Models
+{
+    public class ListarFornecedorViewModel : FornecedorViewModel
+    {
+        public int Produto_id { get; set; } 
+    }
+}
