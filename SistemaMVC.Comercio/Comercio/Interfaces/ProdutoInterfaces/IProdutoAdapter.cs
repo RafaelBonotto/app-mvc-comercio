ed8adc75595436c9874f7911d68222a12d3ce7e8
RafaelBonotto <rafareisaba@outlook.com>
2022-07-30 00:51:39 -0300

    Update IProdutoAdapter.cs

--- SistemaMVC.Comercio/Comercio/Interfaces/ProdutoInterfaces/IProdutoAdapter.cs
+++ SistemaMVC.Comercio/Comercio/Interfaces/ProdutoInterfaces/IProdutoAdapter.cs
@@ -6,7 +6,7 @@ namespace Comercio.Interfaces.ProdutoInterfaces
 {
     public interface IProdutoAdapter
     {
-        Produto MontaProdutoUpdateRepositorio(ProdutoViewModel produtoViewModel, Produto produtoRepositorio);
+        Produto MontaProdutoUpdateRepositorio(ProdutoViewModel produtoViewModel);
         Produto MontaProdutoInsertRepositorio(ProdutoViewModel produtoViewModel);
         ProdutoViewModel MontaProdutoViewModel(Produto produto);
         List<FornecedorViewModel> MontaListaFornecedorViewModel(List<Fornecedor> fornecedores);
