9c90772f0b4d7389133ed463a377f58724bbb821
RafaelBonotto <rafareisaba@outlook.com>
2022-08-13 00:57:57 -0300

    Create ObterFornecedorViewModel.cs

---
+++ SistemaMVC.Comercio/Comercio/Models/ObterFornecedorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Comercio.Models
+{
+    public class ObterFornecedorViewModel 
+    {
+        public int Produto_id { get; set; }
+        public List<FornecedorDescricaoId> Fornecedores { get; set; } = new();
+    }
+
+    public class FornecedorDescricaoId
+    {
+        public int Fornecedor_id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
