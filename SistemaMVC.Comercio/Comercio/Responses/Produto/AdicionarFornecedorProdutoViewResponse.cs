de469cc06021cfbadd2354ad4ec3452a2650d45e
RafaelBonotto <rafareisaba@outlook.com>
2022-08-16 23:05:47 -0300

    Create AdicionarFornecedorProdutoViewResponse.cs

---
+++ SistemaMVC.Comercio/Comercio/Responses/Produto/AdicionarFornecedorProdutoViewResponse.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Comercio.Responses.Produto
+{
+    public class AdicionarFornecedorProdutoViewResponse
+    {
+        public int Produto_id { get; set; } 
+        public string Produto_codigo { get; set; }
+        public string Produto_descricao { get; set; } 
+        public IEnumerable<SelectListItem> FornecedoresBanco { get; set; }
+    }
+}
