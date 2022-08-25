b759e4c1d5bdeae28aa26724b62243d058acacea
RafaelBonotto <rafareisaba@outlook.com>
2022-08-24 23:53:31 -0300

    Create DadosProdutoResponse.cs

---
+++ SistemaMVC.Comercio/Comercio/Responses/Produto/DadosProdutoResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Comercio.Responses.Produto
+{
+    public class DadosProdutoResponse
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Descricao{ get; set; }
+    }
+}
