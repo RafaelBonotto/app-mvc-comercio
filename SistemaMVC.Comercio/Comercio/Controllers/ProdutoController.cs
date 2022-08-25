3282bf4b4b9e05107c7d6d5110107562b2cf9d4a
RafaelBonotto <rafareisaba@outlook.com>
2022-08-25 10:59:38 -0300

    Update ProdutoController.cs

--- SistemaMVC.Comercio/Comercio/Controllers/ProdutoController.cs
+++ SistemaMVC.Comercio/Comercio/Controllers/ProdutoController.cs
@@ -118,7 +118,7 @@ namespace Comercio.Controllers
             try
             {
                 var produtos = await _repositoryProduto.FiltrarPorSetor(setor);
-                if(produtos.Count == 0)
+                if(produtos is null)
                     return View("Error", new ErrorViewModel().ErroFiltroNaoEncontrado());
 
                 var listaViewModel = new List<ProdutoViewModel>();
