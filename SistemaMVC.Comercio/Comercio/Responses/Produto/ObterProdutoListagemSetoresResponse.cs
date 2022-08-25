fcd74fee5776efb0d957bf596119da131bb0e122
RafaelBonotto <rafareisaba@outlook.com>
2022-08-25 16:13:17 -0300

    Create ObterProdutoListagemSetoresResponse.cs

---
+++ SistemaMVC.Comercio/Comercio/Responses/Produto/ObterProdutoListagemSetoresResponse.cs
@@ -0,0 +1,11 @@
+﻿using Comercio.Entities;
+using System.Collections.Generic;
+
+namespace Comercio.Responses.Produto
+{
+    public class ObterProdutoListagemSetoresResponse
+    {
+        public Comercio.Entities.Produto Produto { get; set; }
+        public List<Setor> Setores { get; set; }
+    }
+}
