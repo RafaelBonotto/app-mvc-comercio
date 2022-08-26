93abe529ec285c00d20a2aafd613a28b0ade506e
RafaelBonotto <rafareisaba@outlook.com>
2022-08-26 17:14:22 -0300

    Create ObterFornecedoresDadosProdutoResponse.cs

---
+++ SistemaMVC.Comercio/Comercio/Responses/Produto/ObterFornecedoresDadosProdutoResponse.cs
@@ -0,0 +1,13 @@
+﻿using Comercio.Entities;
+using System.Collections.Generic;
+
+namespace Comercio.Responses.Produto
+{
+    public class ObterFornecedoresDadosProdutoResponse
+    {
+        public int IdProduto { get; set; }
+        public string CodigoProduto { get; set; }
+        public string DescircaoProduto { get; set; }
+        public List<Fornecedor> Fornecedores { get; set; } = new();
+    }
+}
